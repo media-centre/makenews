caff3fab7e11cf67231873fb49270dd6c9ceadd6
Deepthi <deepthirera@gmail.com>
2015-10-29 12:16:27 +0530

    62 [Vin/Dee] required field validation

--- client/src/js/login/Login.js
+++ client/src/js/login/Login.js
@@ -7,12 +7,12 @@ export default class Login extends Component {
           <div className="clear-fix input-container">
 
               <div className="left m-block">
-                  <input type="text" id="user_name" placeholder='username' ref='user_name' name="userName" className="m-input-block box small-text"/>
+                  <input type="text" id="user_name" placeholder='username' ref='user_name' name="userName" className="m-input-block box small-text" required/>
                   <p className="error extra-small-text">Invalid password or password</p>
               </div>
 
               <div className="left m-block password-container">
-                  <input type="password" id="password" placeholder='password' ref='password' name="password" className="m-input-block box small-text"/>
+                  <input type="password" id="password" placeholder='password' ref='password' name="password" className="m-input-block box small-text" required/>
                   <p className="help-login extra-small-text t-right">Need help to login</p>
               </div>
 
