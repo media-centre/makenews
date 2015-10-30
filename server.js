d58aafbf23e47cb04d7b1be243c558fa391e30ef
vinothpro <vinothr@thoughtworks.com>
2015-10-30 12:38:42 +0530

    62 [Vin/Dee] - Basic server logging

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ import routers from './server/src/routes/Routes';
 import routeErrorHandler from "./server/src/routes/RouteErrorHandler.js";
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(morgan('dev'));
+
 routers(app);
 
 const port = process.env.PORT || 5000;
