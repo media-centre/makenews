99408d46b52e73802d0cb3818a672dcb7a19cfd6
vinothpro <vinothr@thoughtworks.com>
2015-11-04 17:48:51 +0530

    83 [Vin/Rav] - Created samle category and handled responsive

--- client/src/js/pages/MainPage/ConfigurePage.jsx
+++ client/src/js/pages/MainPage/ConfigurePage.jsx
@@ -5,8 +5,15 @@ import { connect} from 'react-redux';
 export default class ConfigurePage extends Component {
     render() {
         return (
-            <div>Configure</div>
-
+            <div className="configure-page">
+                <h4 className="t-center">All categories</h4>
+                <div className="categories">
+                    <ul className="cat-list m-t-center">
+                        <li className="add-new"><div className="v-center t-center">Add new category</div></li>
+                        <li className="category"><div className="v-center t-center">Timeline</div></li>
+                    </ul>
+                </div>
+            </div>
         );
     }
 }
