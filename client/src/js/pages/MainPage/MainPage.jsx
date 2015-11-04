da5054b318cf8fab03b6d5de03e10a1e71b8e7d1
vinothpro <vinothr@thoughtworks.com>
2015-11-04 16:52:01 +0530

    83 - [Vin/Rav] - Implemented header in the main page

--- client/src/js/pages/MainPage/MainPage.jsx
+++ client/src/js/pages/MainPage/MainPage.jsx
@@ -6,13 +6,29 @@ import Router, {Route, Link} from 'react-router';
 export default class MainPage extends Component {
     render() {
         return (
-            <div>
+            <div className="main-page">
                 <header>
-                    <ul>
-                        <li><Link to="/configure">Configure</Link></li>
-                        <li><Link to="/surf">Surf</Link></li>
-                        <li><Link to="/park">Park</Link></li>
-                    </ul>
+                    <div className="fixed-header clear-fix multi-column">
+
+                        <div className="app-logo left clear-fix large-text m-none">
+                            <span className="left">make</span>
+                            <b className="left">news</b>
+                        </div>
+
+                        <div className="user-info right">
+                            <a>Logout</a>
+                        </div>
+
+                        <div className="flexible t-center">
+                            <ul className="menu-list">
+                                <li><Link to="/configure"><img src="../../../newspaper.jpg" /><span>Configure</span></Link></li>
+                                <li><Link to="/surf"><img src="../../../newspaper.jpg" /><span>Surf</span></Link></li>
+                                <li><Link to="/park"><img src="../../../newspaper.jpg" /><span>Park</span></Link></li>
+                            </ul>
+                        </div>
+
+                    </div>
+
                 </header>
                 <section>
                 {this.props.children}
