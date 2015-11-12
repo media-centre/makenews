4f0f0af8cbf12da9b50abb480b00e311bca4f5e3
vinothpro <vinothr@thoughtworks.com>
2015-11-12 12:19:26 +0530

    Fixed test navigator error

--- client/test/helper/TestHelper.js
+++ client/test/helper/TestHelper.js
@@ -4,3 +4,4 @@ import React from 'react/addons';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
+global.navigator = global.window.navigator;
