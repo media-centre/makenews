a0c8d198e2f74052c107d8cdcaf6128df6e3b44f
vinothpro <vinothr@thoughtworks.com>
2015-12-07 13:29:43 +0530

    [SSL]Fixed Eslint errors in CategoryNavigationHeader.jsx and RouterLinkHighlighter.js

--- client/src/js/config/components/CategoryNavigationHeader.jsx
+++ client/src/js/config/components/CategoryNavigationHeader.jsx
@@ -1,22 +1,22 @@
-
 /* eslint no-unused-vars:0 */
 "use strict";
 import React, { Component, PropTypes } from "react";
 import { Route, Link } from "react-router";
 import { connect } from "react-redux";
 
-export default class CategoryNavigationHeader extends Component {
+export default
+class CategoryNavigationHeader extends Component {
 
     _validateCategoryTitle(event, props) {
         var categoryName = this.refs.categoryTitleElement.textContent.trim();
-        if(categoryName.toLowerCase() !== this.props.categoryName.toLowerCase()) {
+        if (categoryName.toLowerCase() !== this.props.categoryName.toLowerCase()) {
             props.updateCategoryName(categoryName);
         }
     }
 
     _handleEnterKey(event, props) {
         const ENTERKEY = 13;
-        if(event.keyCode === ENTERKEY) {
+        if (event.keyCode === ENTERKEY) {
             event.preventDefault();
             this.refs.categoryTitleElement.blur(event, props);
         }
@@ -31,17 +31,19 @@ export default class CategoryNavigationHeader extends Component {
 
         let titleElement = this.props.isDefault ? <div className="navigation-title t-center m-block" id="categoryTitle">{this.props.categoryName}</div>
             : <div className="navigation-title t-center m-block custom-category-name">
-                <div className={this._highlightEditableTitle()} id="categoryTitle" ref="categoryTitleElement" contentEditable onKeyDown={(event)=> this._handleEnterKey(event, this.props)} onBlur={(event)=> this._validateCategoryTitle(event, this.props)}>
+            <div className={this._highlightEditableTitle()} id="categoryTitle" ref="categoryTitleElement" contentEditable onKeyDown={(event)=> this._handleEnterKey(event, this.props)} onBlur={(event)=> this._validateCategoryTitle(event, this.props)}>
                     {this.props.categoryName}
-                </div>
-            <div className="error-msg t-center">{this.props.errorMessage}</div></div>;
+            </div>
+            <div className="error-msg t-center">{this.props.errorMessage}</div>
+        </div>;
         let deleteElement = this.props.isDefault ? null : <button className="delete-category right" id="deleteCategory">{"Delete Category"}</button>;
 
         return (
 
             <div className="navigation-header clear-fix">
                 <Link to="/configure/categories" className="navigation nav-control h-center left" id="allCategoriesButton">
-                    <i className="fa fa-arrow-left"></i><span>{"All Categories"}</span>
+                    <i className="fa fa-arrow-left"></i>
+                    <span>{"All Categories"}</span>
                 </Link>
                 {deleteElement}
                 {titleElement}
--- client/src/js/utils/RouterLinkHighlighter.js
+++ client/src/js/utils/RouterLinkHighlighter.js
@@ -1,9 +1,12 @@
+/*eslint no-plusplus: 2*/
+"use strict";
+
 export function highlightRouterLink() {
     let page = window.location.hash.split("/")[1];
     let links = document.querySelectorAll("header ul.menu-list li a");
-    for(let item = 0, length = links.length; item < length; item++) {
+    for (let item = 0, length = links.length; item < length; item += 1) {
         links[item].classList.remove("selected");
-        if(links[item].classList.contains(page)) {
+        if (links[item].classList.contains(page)) {
             links[item].classList.add("selected");
         }
     }
@@ -11,7 +14,7 @@ export function highlightRouterLink() {
 
 export function deHighlightRouterLink() {
     let links = document.querySelectorAll("header ul.menu-list li a");
-    for(let item = 0, length = links.length; item < length; item++) {
+    for(let item = 0, length = links.length; item < length; item += 1) {
         links[item].classList.remove("selected");
     }
 }
