7831b177905bf57a46bed345d7c0c66f88efd901
RaviBolla <ravisrs222@gmail.com>
2015-12-10 16:02:09 +0530

    #92 [Rav/Mur] - Added twitter-feeds route

--- server/src/routes/Routes.js
+++ server/src/routes/Routes.js
@@ -3,10 +3,12 @@ import authorizationRoutes from "./AuthorizationRoutes.js";
 import rssReaderRoutes from "./RssReaderRoutes";
 import twitterReaderRoutes from "./TwitterReaderRoutes";
 import facebookReaderRoutes from "./FacebookRoutes.js";
+import twitterReaderRoutes from "./TwitterReaderRoutes";
 
 export default function(app) {
     authorizationRoutes(app);
     rssReaderRoutes(app);
     twitterReaderRoutes(app);
     facebookReaderRoutes(app);
+    twitterReaderRoutes(app);
 }
