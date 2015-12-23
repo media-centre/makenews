f9170a816daef7e1ab30dfad65fb18cb171a87d0
vinothpro <vinothr@thoughtworks.com>
2015-12-23 16:53:46 +0530

    206 - [Vin] Added tooltip to the park arrow on hover

--- client/src/js/surf/components/SurfFeedActionComponent.jsx
+++ client/src/js/surf/components/SurfFeedActionComponent.jsx
@@ -4,7 +4,7 @@ import React, { Component, PropTypes } from "react";
 export default class SurfFeedActionComponent extends Component {
     render() {
         return (
-            <ul className="surf-actions right clear-fix" onClick={this.props.parkFeed}><li className="left"><i className="fa fa-share"></i></li></ul>
+            <ul className="surf-actions right clear-fix" onClick={this.props.parkFeed}><li className="left" title="Park this feed"><i className="fa fa-share"></i></li></ul>
         );
     }
 }
