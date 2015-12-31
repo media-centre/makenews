d115220d91f965b613dbb81eb47fc2d458f776f8
Anil Kumar Kodi <aniltpl08@gmail.com>
2015-12-31 01:19:12 -0600

    [anil/katta] handled null in posted date

--- client/src/js/surf/components/FeedHeader.jsx
+++ client/src/js/surf/components/FeedHeader.jsx
@@ -5,8 +5,8 @@ import DateTimeUtil from "../../utils/DateTimeUtil.js";
 
 export default class FeedHeader extends Component {
     render() {
-        let localDateTime = DateTimeUtil.getLocalTimeFromUTC(this.props.postedDate);
-        let tags = this.props.tags ? this.props.tags.map((tag, index)=> <li className="tag" key={index}>{tag}</li>) : null;
+        let localDateTime = this.props.postedDate ? DateTimeUtil.getLocalTimeFromUTC(this.props.postedDate) : null;
+        let tags = this.props.tags === "" ? this.props.tags.map((tag, index)=> <li className="tag" key={index}>{tag}</li>) : null;
         let actionElement = this.props.actionComponent ? React.createElement(this.props.actionComponent, this.props) : null;
         return (
             <div className="feed-header border-bottom box h-center clear-fix">
