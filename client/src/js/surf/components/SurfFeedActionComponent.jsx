15b72ac8e80688b49a96b57b7741bf6bfd9adc36
Murali Krishna <muralikv@thoughtworks.com>
2016-01-27 14:47:41 +0530

    #239 [Mur] changed park button icon color in surf page

--- client/src/js/surf/components/SurfFeedActionComponent.jsx
+++ client/src/js/surf/components/SurfFeedActionComponent.jsx
@@ -4,7 +4,11 @@ import React, { Component, PropTypes } from "react";
 export default class SurfFeedActionComponent extends Component {
     render() {
         return (
-            <ul className="feed-actions right clear-fix" onClick={this.props.feedAction}><li className="left" title="Park this feed"><img className="park-feed" src="images/main/park.png" height="10px" width="10px"></img></li></ul>
+            <ul className="feed-actions right clear-fix" onClick={this.props.feedAction}>
+                <li className="left" title="Park this feed">
+                    <div className="park-feed"></div>
+                </li>
+            </ul>
         );
     }
 }
