75b34fcbc3c89d2598a38b744719ade117b33c0f
Deepthi <deepthirera@gmail.com>
2016-02-02 13:17:38 +0530

    #100 [Mah/Dee] Single document for fb and twitter authentication status

--- client/src/js/facebook/FacebookDb.js
+++ client/src/js/facebook/FacebookDb.js
@@ -2,29 +2,9 @@
 
 "use strict";
 import PouchClient from "../db/PouchClient.js";
-import FacebookDocument from "../facebook/FacebookDb.js";
 
 export default class FacebookDb {
     static addFacebookFeeds(feeds) {
         return PouchClient.bulkDocuments(feeds);
-    }
-
-    static getTokenDocument() {
-        return new Promise((resolve, reject) => {
-            PouchClient.getDocument("facebookTokenExpiration").then((document) => {
-                resolve(document);
-            }).catch((error) => {
-                reject(error);
-            });
-        });
-    }
-
-    static updateTokenDocument(document) {
-        PouchClient.updateDocument(document);
-    }
-
-    static createTokenDocument(expiredAfter) {
-        let document = { "_id": "facebookTokenExpiration", "expiredAfter": expiredAfter };
-        PouchClient.createDocument(document);
     }
 }
