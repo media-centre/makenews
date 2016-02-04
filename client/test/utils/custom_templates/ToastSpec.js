3d8f09ae06c0d5140b73dd887160d5ce48826f90
RaviBolla <ravisrs222@gmail.com>
2016-02-04 16:08:34 +0530

    [Mur] restored clock mock in ToastSpec

--- client/test/utils/custom_templates/ToastSpec.js
+++ client/test/utils/custom_templates/ToastSpec.js
@@ -5,32 +5,43 @@ import sinon from "sinon";
 import { assert } from "chai";
 
 describe("Toast", () => {
-    it("should have Toast template in the document", () => {
-        Toast.show("Category created!!");
-        assert.isNotNull(document.getElementById("custom-toast"), "Toast template is available");
-    });
+    describe("show", () => {
+        let sandbox = null;
+        beforeEach("show", () => {
+            sandbox = sinon.sandbox.create();
+        });
 
-    it("should be hidden in 5 seconds", () => {
-        let clock = sinon.useFakeTimers();
-        Toast.show("Category created!!");
-        let time = 5000;
-        clock.tick(time);
-        assert.isTrue(document.getElementById("custom-toast").classList.contains("hide"));
-    });
+        afterEach("show", () => {
+            sandbox.restore();
+        });
 
-    it("should reuse the toast when it is called multiple times", ()=> {
-        let clock = sinon.useFakeTimers();
-        Toast.show("Category created!!");
-        let time = 2000;
-        clock.tick(time);
-        Toast.show("Category re-created!!");
-        assert.isFalse(document.getElementById("custom-toast").classList.contains("hide"));
-        assert.strictEqual("Category re-created!!", document.querySelector("#custom-toast .message").textContent);
-    });
+        it("should have Toast template in the document", () => {
+            Toast.show("Category created!!");
+            assert.isNotNull(document.getElementById("custom-toast"), "Toast template is available");
+        });
 
-    it("should close the toast by clicking close button", ()=> {
-        Toast.show("Category created!!");
-        Toast.close();
-        assert.isTrue(document.getElementById("custom-toast").classList.contains("hide"));
+        it("should be hidden in 5 seconds", () => {
+            let clock = sandbox.useFakeTimers();
+            Toast.show("Category created!!");
+            let time = 5000;
+            clock.tick(time);
+            assert.isTrue(document.getElementById("custom-toast").classList.contains("hide"));
+        });
+
+        it("should reuse the toast when it is called multiple times", ()=> {
+            let clock = sandbox.useFakeTimers();
+            Toast.show("Category created!!");
+            let time = 2000;
+            clock.tick(time);
+            Toast.show("Category re-created!!");
+            assert.isFalse(document.getElementById("custom-toast").classList.contains("hide"));
+            assert.strictEqual("Category re-created!!", document.querySelector("#custom-toast .message").textContent);
+        });
+
+        it("should close the toast by clicking close button", ()=> {
+            Toast.show("Category created!!");
+            Toast.close();
+            assert.isTrue(document.getElementById("custom-toast").classList.contains("hide"));
+        });
     });
 });
