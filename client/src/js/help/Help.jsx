b2f9cb05c188c5981c459eefcf57fdc9e211ba44
Murali Krishna <vmurali1444@gmail.com>
2016-03-03 20:28:24 +0530

    [Mur] - Chnaged the youtube url, slide share url to proper urls in help page

--- client/src/js/help/Help.jsx
+++ client/src/js/help/Help.jsx
@@ -13,13 +13,13 @@ export default class Help extends Component {
                 <h3 className="t-center t-bold"> {"Help"} </h3>
                 <div className="help">
                     <div className="help-box help-youtube">
-                        <iframe src="https://www.youtube.com/embed/UCXGei8H1W8" frameBorder="0" allowFullScreen>{"Video"}</iframe>
+                        <iframe src="https://www.youtube.com/embed/vbklyvS9VnA" frameBorder="0" allowFullScreen>{"Video"}</iframe>
                         <div className="t-center help-text">
                             <i className="fa fa-youtube fa-3x"></i>
                         </div>
                     </div>
                     <div className="help-box help-slide">
-                        <iframe src="//www.slideshare.net/slideshow/embed_code/key/3uMZsTkvjU99xY" frameBorder="0" allowFullScreen>{"PDF"}</iframe>
+                        <iframe src="//www.slideshare.net/slideshow/embed_code/key/eviKUGwN09K1u7" frameBorder="0" allowFullScreen>{"PDF"}</iframe>
                         <div className="t-center help-text">
                             <i className="fa fa-file-pdf-o fa-3x"></i>
                         </div>
