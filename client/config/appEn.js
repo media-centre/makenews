5ca270c250a8403eff6ca75ee796e4cd817a2b15
RaviBolla <ravisrs222@gmail.com>
2016-03-14 16:42:06 +0530

    [Rav] updated branding and features text on login page

--- client/config/appEn.js
+++ client/config/appEn.js
@@ -16,32 +16,20 @@ window.mediaCenter.appEn = {
 
             },
             "branding": {
-                "text": "MakeNews gives you an aggregate idea of the happenings in the world. \
-                News from major news outlets and independent media, reports from press agencies, opinions, columns and articles from \
-                favourite writers, as well as social media posts are seen in one space. \
-                You identify your sources on the Web, Twitter and Facebook, MakeNews relays you updates. \
-                You do not have to go to each and every source to seek updates. Nifty features like Park help you set aside interesting posts \
-                from the rush of news items. You can also group your sources into Categories and get a taxonomically arranged view of updates. \
-                MakeNews is aimed at independent journalists, reporters, editors and newsrooms. \
-                It serves as a powerful journalistic tool that enables users get a broad view of the rapidly updating, \
-                vast sea of current happenings on the web and social media."
+                "text": "MakeNews is for journalists and newsrooms. It helps you track news from web and social media in real-time."
             },
             "featuresHelp": {
                 "configureHelp": {
                     "name": "Configure",
-                    "text": "You identify your favourite RSS feeds, Twitter handles, Twitter hashtags and Facebook pages as sources. \
-                    Group your sources into Categories and get a taxonomically arranged view of news updates."
+                    "text": "You identify your favourite RSS feeds, Twitter handles, Twitter hashtags and Facebook pages as sources. Group your sources into Categories and get an structured view of news updates."
                 },
                 "surfHelp": {
                     "name": "Surf",
-                    "text": "MakeNews relays you updates from sources. You do not have to go to each and every source yourself to seek updates. \
-                    You can scroll infinitely through regular updates from web, twitter and facebook sources that you have added using Configure. \
-                    Click on an item and it opens up in a new tab."
+                    "text": "MakeNews relays updates from sources. You do not have to go to every source to seek updates. Add sources in Configure then scroll infinitely through updates in Surf. Click on the item and it opens up in a new tab."
                 },
                 "parkHelp": {
                     "name": "Park",
-                    "text": "Set aside interesting posts from the rush of news items. Parked items can be considered later or unparked and \
-                    moved back to Surf."
+                    "text": "Set aside interesting posts from the rush of news items in Surf. Parked items can be considered later or unparked and moved back to Surf."
                 }
             }
         },
