6e745bb3f83a86326c02f60e2d655a61554218d9
Mahitha Arveti <marveti@thoughtworks.com>
2016-11-15 12:54:52 +0530

    [swe/Mahitha] added spec for rss component and changed admin credeantials in application.json

--- client/test/config/components/RSSComponentSpec.jsx
+++ client/test/config/components/RSSComponentSpec.jsx
@@ -1,2 +1,62 @@
 "use strict";
-import RSSComponent from "../../../src/js/config/components/RSSComponent.jsx"; //eslint-disable-line no-unused-vars
+import RSSComponent from "../../../src/js/config/components/RSSComponent.jsx";
+import {assert} from "chai";
+import * as sinon from "sinon"; //eslint-disable-line no-unused-vars
+
+describe("Rss Component", () => {
+
+    let content = null, categoryDetailsPageStrings = null;
+    let rssComponent;
+
+    before("rss Component", () => {
+        content = [];
+        categoryDetailsPageStrings = {
+            "hintMessages": {
+                "RSSHintMessage": "rss hint"
+            },
+            "exampleMessages": {
+                "RSSExampleURL": "rss example url"
+            },
+        };
+        rssComponent = TestUtils.renderIntoDocument(
+            <RSSComponent content={content}
+                          categoryDetailsPageStrings={categoryDetailsPageStrings }
+                          dispatch={ () => {
+                          }}/>
+        );
+    });
+
+
+    it("should have RSSHintMessage ", () => {
+        let rssComponentDOM = rssComponent.props.categoryDetailsPageStrings.hintMessages.RSSHintMessage;
+        assert.strictEqual("rss hint", rssComponentDOM);
+    });
+
+    it("should have RSSExampleUrl ", () => {
+        let rssComponentDOM = rssComponent.props.categoryDetailsPageStrings.exampleMessages.RSSExampleURL;
+        assert.strictEqual("rss example url", rssComponentDOM);
+
+    });
+    it("should call dispath function in validate  ", () => {
+
+        let fun = () => {
+
+        };
+        fun = sinon.spy();
+        let props =
+        {
+            "content": [],
+            "categoryDetailsPageStrings": {
+                "hintMessages": {"RSSHintMessage": "hint message"},
+                "exampleMessages": {"RSSExampleURL": "example message"}
+            },
+
+            "dispatch": fun
+
+        };
+        let newob = new RSSComponent(props);
+        newob._validateUrl("https://thehindu.com", fun, props);
+        assert.isTrue(fun.called);
+    });
+});
+
