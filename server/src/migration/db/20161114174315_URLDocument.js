6a054d1095b0466bc4f3da33e18000f0ed65e749
Vankayalapati Nagadurga <nvankaya@thoughtworks.com>
2016-11-16 17:49:08 +0530

    [Sai/Dur] - Adding migration scripts for the default URL Documents

--- server/src/migration/db/20161114174315_URLDocument.js
+++ server/src/migration/db/20161114174315_URLDocument.js
@@ -1,18 +1,17 @@
-"use strict";
-import HttpResponseHandler from "../../../../common/src/HttpResponseHandler.js";
-import ApplicationConfig from "../../config/ApplicationConfig.js";
-import NodeErrorHandler from "../../NodeErrorHandler.js";
-import Migration from "../Migration.js";
+import DesignDocumentMigration from "../helpers/DesignDocumentMigration";
+import Migration from "../Migration";
+import CouchClient from "../../CouchClient";
+import HttpResponseHandler from "../../../../common/src/HttpResponseHandler";
+import ApplicationConfig from "../../config/ApplicationConfig";
+import NodeErrorHandler from "../../NodeErrorHandler";
 import request from "request";
-import fs from "fs";
-import path from "path";
 
 export default class URLDocument {
     constructor(dbName, accessToken) {
         this.dbName = dbName;
-        this.accessToken = accessToken;
+        this.designDocumentMigObj = DesignDocumentMigration.instance(dbName, accessToken, "_design/category");
+        this.couchClient = CouchClient.instance(dbName, accessToken);
     }
-
     getDocument() {
         return JSON.parse(fs.readFileSync(path.join(__dirname, "../documents/defaultURLDocument.json"), "utf-8"));  //eslint-disable-line no-sync
     }
@@ -20,28 +19,45 @@ export default class URLDocument {
     up() {
         return new Promise((resolve, reject) => {
             Migration.logger(this.dbName).info("URLDocument::up - started");
-            let categoryDocument = this.getDocument();
-            request.put({
-                    "uri": ApplicationConfig.instance().dbUrl() + "/" + this.dbName + "/_all_docs?include_docs=true",
-                    "headers": { "Cookie": "AuthSession=" + this.accessToken, "Content-Type": "application/json", "Accept": "application/json" },
-                    "body": categoryDocument,
-                    "json": true
-                },
-                (error, response) => {
-                    if(NodeErrorHandler.noError(error)) {
-                        if(new HttpResponseHandler(response.statusCode).success()) {
-                            Migration.logger(this.dbName).debug("URLDocument::up - success %j", response.body);
-                            resolve(response.body);
+            this.designDocumentMigObj.addOrUpdateViews(this._filterViews()).then(res => {
+                Migration.logger(this.dbName).info("CreateDefaultCategoryDocument::up - started");
+                let categoryDocument = this.getDocument();
+                request.post({
+                        "uri": ApplicationConfig.instance().dbUrl() + "/" + this.dbName + "/_bulk_docs",
+                        "headers": { "Cookie": "AuthSession=" + this.accessToken, "Content-Type": "application/json", "Accept": "application/json" },
+                        "body": categoryDocument,
+                        "json": true
+                    },
+                    (error, response) => {
+                        if(NodeErrorHandler.noError(error)) {
+                            if(new HttpResponseHandler(response.statusCode).success()) {
+                                Migration.logger(this.dbName).debug("CreateDefaultCategoryDocument::up - response %j", response.body);
+                                resolve(response.body);
+                            } else {
+                                Migration.logger(this.dbName).error("CreateDefaultCategoryDocument::up - error %j", response.body);
+                                reject("unexpected response from the db");
+                            }
                         } else {
-                            Migration.logger(this.dbName).error("URLDocument::up - error %j", response.body);
-                            reject("unexpected response from the db");
+                            Migration.logger(this.dbName).error("CreateDefaultCategoryDocument::up - error %j", error);
+                            reject(error);
                         }
-                    } else {
-                        Migration.logger(this.dbName).error("URLDocument::up - error %j", error);
-                        reject(error);
-                    }
-                });
+                    });
+                console.log("HEllo qorld");
+                console.log(res);
+                resolve(res);
+            }).catch(error => {
+                Migration.logger(this.dbName).error("URLDocument::up - error %j", error);
+                reject(error);
+            });
         });
     }
-}
 
+    _filterViews() {
+        return {
+            "defaultURLDocuments": {
+                "map": "function(doc) { if(doc.sourceType == 'web') {emit(doc._id, doc)} }"
+            }
+        };
+
+    }
+}
