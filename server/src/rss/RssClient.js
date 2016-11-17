f05b06b4bff3b9c6d4259f73d87aa09bcc28aa8f
swethab <swethab@thoughtworks.com>
2016-11-17 18:32:51 +0530

    More tests on rssClient

--- server/src/rss/RssClient.js
+++ server/src/rss/RssClient.js
@@ -23,107 +23,43 @@ export default class RssClient {
             return feeds;
         }
         catch (error) {
-            console.log("1")
             if (error.message === FEEDS_NOT_FOUND) {
-                console.log("2")
                 let root = cheerio.load(error.data);
                 let rssLink = root("link[type ^= 'application/rss+xml']");
+                let rssUrl = rssLink.attr("href");
+                console.log(rssUrl + url)
                 if (rssLink && rssLink.length !== 0) {
-                    console.log("3")
-                    let rssUrl = rssLink.attr("href");
-                    if (rssUrl.startsWith("//")) {
-                        rssUrl = url.substring(0, url.indexOf("//")) + rssUrl;
-                    } else if (rssUrl.startsWith("/")) {
-                        rssUrl = url.substring(0, url.indexOf("/", httpIndex)) + rssUrl;
-                    }
-                    try {
-                        let feeds = await this.getRssData(rssUrl);
-                        feeds.url = rssUrl;
-                        return feeds;
-                    }
-                    catch (rssError) {
-                        return this.handleRequestError(url, rssError);
-                    }
+                    return this.getFeedsFromRssUrl(rssUrl,url)
 
                 } else {
-                    console.log("4")
                     return this.crawlForRssUrl(root, url.replace(/\/+$/g, ""));
                 }
             } else {
-                console.log("5")
                 this.handleUrlError(url, error);
             }
 
         }
-        //return new Promise((resolve, reject) => {
-        //    this.getRssData(url).then(feeds => {
-        //        resolve(feeds);
-        //    }).catch(error => {
-        //        if(error.message === FEEDS_NOT_FOUND) {
-        //            let root = cheerio.load(error.data);
-        //            let rssLink = root("link[type ^= 'application/rss+xml']");
-        //            if (rssLink && rssLink.length !== 0) {
-        //                let rssUrl = rssLink.attr("href");
-        //                if(rssUrl.startsWith("//")) {
-        //                    rssUrl = url.substring(0, url.indexOf("//")) + rssUrl;
-        //                } else if(rssUrl.startsWith("/")) {
-        //                    rssUrl = url.substring(0, url.indexOf("/", httpIndex)) + rssUrl;
-        //                }
-        //                this.getRssData(rssUrl).then(feeds => {
-        //                    feeds.url = rssUrl;
-        //                    resolve(feeds);
-        //                }).catch(rssError => {
-        //                    this.handleRequestError(url, rssError, reject);
-        //                });
-        //            } else {
-        //                this.crawlForRssUrl(root, url.replace(/\/+$/g, ""), resolve, reject);
-        //            }
-        //        } else {
-        //            this.handleUrlError(url, error, reject);
-        //        }
-        //    });
-        //});
+
+    }
+
+    async getFeedsFromRssUrl(rssUrl,url){
+
+        if (rssUrl.startsWith("//")) {
+            rssUrl = url.substring(0, url.indexOf("//")) + rssUrl;
+        } else if (rssUrl.startsWith("/")) {
+            rssUrl = url.substring(0, url.indexOf("/", httpIndex)) + rssUrl;
+        }
+        try {
+            let feeds = await this.getRssData(rssUrl);
+            feeds.url = rssUrl;
+            return feeds;
+        }
+        catch (rssError) {
+            return this.handleRequestError(url, rssError);
+        }
     }
 
     async crawlForRssUrl(root, url) {
-        // console.log("6")
-
-        // let links = new Set();
-        // let rssUrl = url.substring(0, url.indexOf("/", httpIndex));
-        // let relativeLinks = root("a[href^='/']");
-        // relativeLinks.each(function () {
-        //     links.add(rssUrl + root(this).attr("href"));
-        // });
-        //
-        // rssUrl = rssUrl.replace(/.*?:\/\//g, "");
-        // rssUrl = rssUrl.replace("www.", "");
-        // let absoluteLinks = root("a[href*='" + rssUrl + "']");
-        // absoluteLinks.each(function () {
-        //     links.add(root(this).attr("href"));
-        // });
-        //
-        // if (links.size === 0) {
-        //     return this.handleUrlError(url, "no rss links found");
-        // } else {
-        //     let count = 0;
-        //     for (link of links) {
-        //         try {
-        //             let feeds = await this.getRssData(link, url);
-        //             feeds.url = link;
-        //             return feeds;
-        //         }
-        //         catch (error) {
-        //             count += 1;
-        //             if (count === links.size) {
-        //                 return this.handleUrlError(link, error);
-        //             }
-        //             return this.crawlRssList(link, error, url);
-        //
-        //         }
-        //         ;
-        //     }
-        //     ;
-        // }
         let links = new Set();
         let rssUrl = url.substring(0, url.indexOf("/", httpIndex));
         let relativeLinks = root("a[href^='/']");
@@ -142,17 +78,20 @@ export default class RssClient {
             this.handleUrlError(url, "no rss links found", reject);
         } else {
             let count = 0;
-            links.forEach((link) => {
-                this.getRssData(link, false).then(feeds => {
+            links.forEach(async (link) => {
+                try {
+                    let feeds = await this.getRssData(link, false);
                     feeds.url = link;
                     resolve(feeds);
-                }).catch(error => {
+                }
+                catch(error)  {
                     count += 1;
-                    this.crawlRssList(link, error, url, resolve);
                     if (count === links.size) {
-                        this.handleUrlError(link, error, reject);
+                        return this.handleUrlError(link, error, reject);
                     }
-                });
+                    return this.crawlRssList(link, error, url, resolve);
+
+                }
             });
         }
     }
@@ -174,57 +113,9 @@ export default class RssClient {
         }
     }
 
-    // crawlRssList(link, error, url, resolve)
-    // let rssPath = link.substring(link.lastIndexOf("/"));
-    // if ((rssPath.indexOf("rss")) !== NOT_FOUND_INDEX) {
-    //     let rssListRoot = cheerio.load(error.data);
-    //     let urlPath = url.substring(url.lastIndexOf("/"));
-    //     urlPath = urlPath.replace(/\.[\w]*$/g, "");
-    //     let rssListLink = rssListRoot("a[href$='" + urlPath + "']").attr("href") ||
-    //         rssListRoot("a[href$='" + urlPath + ".rss']").attr("href") ||
-    //         rssListRoot("a[href$='" + urlPath + ".xml']").attr("href");
-    //     if (rssListLink) {
-    //         this.getRssData(rssListLink, false).then(rssFeeds => {
-    //             rssFeeds.url = rssListLink;
-    //             resolve(rssFeeds);
-    //         });
-    //     }
-    // }
 
 
-     async getRssData(url) {
-         // return new Promise((resolve, reject) => {
-         //     let data = null;
-         //     let requestToUrl = request.get({
-         //         "uri": url,
-         //         "timeout": 6000
-         //     }, (error, response, body) => {
-         //         if (error) {
-         //             this.handleRequestError(url, error, reject);
-         //         }
-         //         data = body;
-         //     });
-         //     requestToUrl.on("response", async function (res) {
-         //         if (res.statusCode !== HttpResponseHandler.codes.OK) {
-         //             RssClient.logger().error("RssClient:: %s returned invalid status code '%s'.", res.statusCode);
-         //             reject({"message": "Bad status code"});
-         //         }
-         //         let rssParser = new RssParser(this);
-         //         try {
-         //             let feeds = await rssParser.parse();
-         //             RssClient.logger().debug("RssClient:: successfully fetched feeds for %s.", url);
-         //             return feeds;
-         //         }
-         //         catch (error) {
-         //             RssClient.logger().error("RssClient:: %s is not a proper feed url. Error: %s.", url, error);
-         //             return ({"message": FEEDS_NOT_FOUND, "data": data});
-         //         }
-         //
-         //
-         //     });
-         // });
-
-
+      getRssData(url) {
 
         return new Promise((resolve, reject) => {
             let data = null;
--- server/test/rss/RssClientSpec.js
+++ server/test/rss/RssClientSpec.js
@@ -30,7 +30,7 @@ describe("fetchRssFeeds", () => {
 
     });
 
-    it("should call handleError when error message is other than FEEDS_NOT_FOUND ", () => {
+    it.only("should call handleError when error message is other than FEEDS_NOT_FOUND ", () => {
         let rssClientMock =new RssClient();
         let error =[{
             "message": "new error"
@@ -39,6 +39,8 @@ describe("fetchRssFeeds", () => {
         getrssMock.returns(Promise.reject({ "message": "Bad status code" }));
         let handleMock = sinon.mock(rssClientMock).expects("handleUrlError");
         rssClientMock.fetchRssFeeds("www.error.com").catch(error => {
+            console.log("test")
+
             handleMock.verify();
             rssClientMock.getRssData.restore();
             rssClientMock.handleUrlError.restore();
@@ -57,6 +59,59 @@ describe("fetchRssFeeds", () => {
             rssClientMock.getRssData.restore();
             rssClientMock.crawlForRssUrl.restore();
         })
+
+    });
+
+    it("should call getFeedsFromRssUrl when feeds are  present", () => {
+            let rssClientMock =new RssClient();
+            let getrssMock = sinon.mock(rssClientMock).expects("getRssData").withArgs("www.error.com");
+            getrssMock.returns(Promise.reject({ "message": "feeds_not_found", "data": '<link type="application/rss+xml" href="http://www.example.com"> <a href="http://www.example.com" ></a> ' }));
+            let feed =[{
+            "_id": "A7AE6BD7-0B65-01EF-AE07-DAE4727754E3",
+            "_rev": "1-a1fc119c81b2e042c1fe10721af7ac56",
+            "docType": "source",
+            "sourceType": "twitter",
+            "url": "@balaswecha",
+            "categoryIds": [
+                "95fa167311bf340b461ba414f1004074"
+            ],
+            "status": "valid"
+             }];
+        let getrssMockNew = sinon.mock(rssClientMock).expects("getFeedsFromRssUrl");
+        getrssMockNew.returns(feed);
+        return Promise.resolve(rssClientMock.fetchRssFeeds("www.error.com")).then(result => {
+            assert.deepEqual(result, feed);
+            rssClientMock.getRssData.restore();
+            rssClientMock.getFeedsFromRssUrl.restore();
+        })
+
+        });
+
+    it.only("should call handleRequestError when rss data is not present", () => {
+        let rssClientMock =new RssClient();
+        let getrssMock = sinon.mock(rssClientMock).expects("getRssData").withArgs("www.error.com");
+        getrssMock.returns(Promise.reject({ "message": "feeds_not_found", "data": '<link type="application/rss+xml" href="http://www.error.com"> ' }));
+        let feed =[{
+        "_id": "A7AE6BD7-0B65-01EF-AE07-DAE4727754E3",
+        "_rev": "1-a1fc119c81b2e042c1fe10721af7ac56",
+        "docType": "source",
+        "sourceType": "twitter",
+        "url": "@balaswecha",
+        "categoryIds": [
+            "95fa167311bf340b461ba414f1004074"
+        ],
+        "status": "valid"
+         }];
+        let handleRequestMock = sinon.mock(rssClientMock).expects("handleRequestError");
+
+        return Promise.reject(rssClientMock.getFeedsFromRssUrl("http://www.example.com", "www.error.com")).catch(error => {
+            console.log("*************" );
+            handleRequestMock.verify();
+        assert.deepEqual(error.message, "feedot_found");
+        rssClientMock.getRssData.restore();
+        rssClientMock.handleRequestError.restore();
+    })
+
 
     });
 
