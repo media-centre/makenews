67d361fd51f05382e7eabb4283d9a863372b6ed1
Saisree <saisree.sree9@gmail.com>
2016-11-28 11:56:42 +0530

    [Mah/SSL] - changing promises to async and await in RssRequestHandler and RssClient

--- server/src/routes/Routes.js
+++ server/src/routes/Routes.js
@@ -3,7 +3,6 @@ import rssReaderRoutes from "./RssRoutes";
 import facebookReaderRoutes from "./FacebookRoutes";
 import twitterReaderRoutes from "./TwitterRoutes";
 import fetchAllFeedsRoutes from "../fetchAllFeeds/FetchFeedsFromAllSourcesRoutes";
-import webURLsRoutes from "./WebURLsRoutes";
 
 export default function(app) {
     authorizationRoutes(app);
@@ -11,5 +10,4 @@ export default function(app) {
     facebookReaderRoutes(app);
     twitterReaderRoutes(app);
     fetchAllFeedsRoutes(app);
-    webURLsRoutes(app);
 }
