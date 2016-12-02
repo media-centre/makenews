b469e189e9ba053059f42b78bcaf03afbc0bf8b5
Saisree <saisree.sree9@gmail.com>
2016-12-02 10:49:46 +0530

    [#40- SSL/Mht] - changed query for displaying feeds from db

--- client/src/js/newsboard/reducers/NewsBoardReducer.js
+++ client/src/js/newsboard/reducers/NewsBoardReducer.js
@@ -1,7 +1,7 @@
 import { DISPLAY_FETCHED_FEEDS } from "../actions/NewsBoardActions";
 import { List } from "immutable";
 
-export function fetchedFeeds(state = { }, action = {}) {
+export function fetchedFeeds(state = {}, action = {}) {
     switch (action.type) {
     case DISPLAY_FETCHED_FEEDS:
         return Object.assign({}, state, {
