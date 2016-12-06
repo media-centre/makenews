30eff22eb1ed7e8926da1d69f2ad68f66a05ff7d
Saisree <saisree.sree9@gmail.com>
2016-12-07 01:48:03 +0530

    [#40 - SSL] - styling changes for feeds on newsboard page

--- client/src/js/newsboard/components/Feed.jsx
+++ client/src/js/newsboard/components/Feed.jsx
@@ -1,17 +1,18 @@
 import React, { Component, PropTypes } from "react";
 import DateTimeUtil from "../../utils/DateTimeUtil";
+import getHtmlContent from "../../utils/HtmContent";
 
 export default class Feed extends Component {
     render() {
         return (<div className={this.props.active ? "feed-highlight" : "feed"} onClick={this.props.onToggle}>
             <div className={this.props.feed.enclosures[0] ? "feed-content-withImg" : "feed-content-noImg"}>
                 <div className="feed-title">{this.props.feed.title}</div>
-                <div className="feed-description">{this.props.feed.description}</div>
+                <div className="feed-description">{getHtmlContent(this.props.feed.description)}</div>
 
             </div>
-            <div className="feed-image"><img src={this.props.feed.enclosures[0] ? this.props.feed.enclosures[0].url : null}/></div>
+            <div className="feed-image">{this.props.feed.enclosures[0] ? <img src={this.props.feed.enclosures[0].url}/> : null }</div>
             <div className="feed-source">
-                <div className="source-type">{this.props.feed.sourceType === "rss" ? <i className="fa fa-globe"/> : <i className={"fa fa-" + this.props.feed.sourceType.toLowerCase()} />}</div>
+                <div className="source-type">{this.props.feed.sourceType === "rss" ? <i className="fa fa-globe"/> : <i className={"fa fa-" + this.props.feed.sourceType.toLowerCase()}/>}</div>
                 <div className="source">{this.props.feed.tags[0]}</div>
                 <div className="date">{DateTimeUtil.getLocalTimeFromUTC(this.props.feed.pubDate)}</div>
 
