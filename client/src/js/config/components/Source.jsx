3dc597461b8edfb82b3918f27e625e2bf2b7195c
Murali Krishna <vmurali1444@gmail.com>
2016-12-16 14:24:16 +0530

    [#16 - Mur/Mht] - displaying url for the web source in source results

--- client/src/js/config/components/Source.jsx
+++ client/src/js/config/components/Source.jsx
@@ -7,14 +7,22 @@ export default class Source extends Component {
         return (
             <div className={this.props.source.added ? "source added" : "source"}>
                 <div className="source__icon">
-                    { this.props.source.picture ?
+                    { this.props.source.picture &&
                         <img src={this.props.source.picture.data.url} width="40px" height="40px"/>
-                        : <img src="./images/default-source-icon.jpg" width="40px" height="40px"/>
                     }
                 </div>
-                <div className="source__title">
-                    { this.props.source.name }
+                <div className="source__details">
+                    <div className="source__title">
+                        { this.props.source.name }
+                    </div>
+
+                    { this.props.source.url &&
+                    <div className="source__url">
+                        { this.props.source.url }
+                    </div>
+                    }
                 </div>
+
                 { this.props.source.added ?
                     (<div className="source__added-icon source__action-icon">
                         <img src="./images/success-arrow.png"/>
