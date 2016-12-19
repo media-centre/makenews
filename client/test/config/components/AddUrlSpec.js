1385edee7cbd48476562d9631317c9c0a69649c5
Saisree <saisree.sree9@gmail.com>
2016-12-19 18:17:36 +0530

    Fixed merge conflicts

--- client/test/config/components/AddUrlSpec.js
+++ client/test/config/components/AddUrlSpec.js
@@ -1,23 +1,23 @@
-import AddUrl from "./../../../src/js/config/components/AddUrl";
-import React from "react";
-import TestUtils from "react-addons-test-utils";
-
-xdescribe("Add Url", () => {
-    let addUrlDom = null, store = null;
-
-    beforeEach("Add Url", () => {
-        let dispatch = () => {};
-        let message = "some";
-        store = { "getState": () => {
-            return {
-                "addUrlMessage": message
-            };
-        } };
-
-        let renderer = TestUtils.createRenderer();
-        addUrlDom = renderer.render(<AddUrl store={store} dispatch={dispatch} message={message}/>);
-    });
-
-   it("should have help message", () => {
-   });
-});
+////import AddUrl from "./../../../src/js/config/components/AddUrl";
+////import React from "react";
+////import TestUtils from "react-addons-test-utils";
+//
+//xdescribe("Add Url", () => {
+//   // let addUrlDom = null, store = null;
+//   //
+//   // beforeEach("Add Url", () => {
+//   //     let dispatch = () => {};
+//   //     let message = "some";
+//   //     store = { "getState": () => {
+//   //         return {
+//   //             "addUrlMessage": message
+//   //         };
+//   //     } };
+//   //
+//   //     let renderer = TestUtils.createRenderer();
+//   //     addUrlDom = renderer.render(<AddUrl store={store} dispatch={dispatch} message={message}/>);
+//   // });
+//   //
+//   //it("should have help message", () => {
+//   //});
+//});
