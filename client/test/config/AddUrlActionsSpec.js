a2ca15a7acd92cb5854f1f0b9cb440e582ae61af
Vankayalapati Nagadurga <nvankaya@thoughtworks.com>
2016-12-21 11:12:29 +0530

    [#26]- Added test cases for Add url

--- client/test/config/AddUrlActionsSpec.js
+++ client/test/config/AddUrlActionsSpec.js
@@ -1,4 +1,4 @@
-import { addRssUrl, MESSAGE } from "./../../src/js/config/actions/AddUrlActions";
+import { addRssUrl, invalidRssUrl, MESSAGE } from "./../../src/js/config/actions/AddUrlActions";
 import AjaxClient from "./../../src/js/utils/AjaxClient";
 import mockStore from "./../helper/ActionHelper";
 import { assert } from "chai";
@@ -37,5 +37,48 @@ describe("AddUrl Actions", () => {
             ajaxClientMock.verify();
             postMock.verify();
         }));
+    });
+
+    it("should return already exist message if url is exist in databse", (done) => {
+        message = "URL already exist";
+        url = "http://newsclick.in/taxonomy/term/economy/feed";
+
+        postMock.expects("post").withArgs(headers, { "url": url }).returns(Promise.resolve(message));
+
+        let action = [{ "type": MESSAGE, message }];
+        let store = mockStore([], action, done);
+        store.dispatch(addRssUrl(url, (result) => {
+            assert.deepEqual(result, message);
+            ajaxClientMock.verify();
+            postMock.verify();
+        }));
+    });
+
+    it("should return invalid message if url is invalid", (done) => {
+        message = "Invalid RSS URL. Please check the URL";
+        url = "http://x.com";
+
+        postMock.expects("post").withArgs(headers, { "url": url }).returns(Promise.reject(message));
+
+        let action = [{ "type": MESSAGE, message }];
+        let store = mockStore([], action, done);
+        store.dispatch(addRssUrl(url, (result) => {
+            assert.deepEqual(result, message);
+            ajaxClientMock.verify();
+            postMock.verify();
+        }));
+    });
+
+    describe("invalid RssUrl", () => {
+        it("should return message", () => {
+            message = "Please enter proper url.";
+            let action = [{ "type": MESSAGE, message }];
+
+            let store = mockStore([], action);
+
+            store.dispatch(invalidRssUrl(url, (result) => {
+                assert.deepEqual(result, message);
+            }));
+        });
     });
 });
--- client/test/config/components/AddUrlSpec.js
+++ client/test/config/components/AddUrlSpec.js
@@ -1,23 +1,32 @@
-////import AddUrl from "./../../../src/js/config/components/AddUrl";
-////import React from "react";
-////import TestUtils from "react-addons-test-utils";
 //
-//xdescribe("Add Url", () => {
-//   // let addUrlDom = null, store = null;
-//   //
-//   // beforeEach("Add Url", () => {
-//   //     let dispatch = () => {};
-//   //     let message = "some";
-//   //     store = { "getState": () => {
-//   //         return {
-//   //             "addUrlMessage": message
-//   //         };
-//   //     } };
-//   //
-//   //     let renderer = TestUtils.createRenderer();
-//   //     addUrlDom = renderer.render(<AddUrl store={store} dispatch={dispatch} message={message}/>);
-//   // });
-//   //
-//   //it("should have help message", () => {
-//   //});
-//});
+// import AddUrl from "./../../../src/js/config/components/AddUrl";
+// import * as AddUrlActions from "./../../../src/js/config/actions/AddUrlActions";
+// import React from "react";
+// import TestUtils from "react-addons-test-utils";
+// import { applyMiddleware, createStore } from "redux";
+// import thunkMiddleware from "redux-thunk";
+// import { Provider } from "react-redux";
+// import sinon from "sinon";
+//
+// describe("Add Url", () => {
+//     let addUrlDom = null, store = null, sandbox = null;
+//
+//     beforeEach("Add Url", () => {
+//         sandbox = sinon.sandbox.create();
+//         let message = "some";
+//         store = createStore(() => ({
+//             "addUrlMessage": message
+//         }), applyMiddleware(thunkMiddleware));
+//
+//         sandbox.mock(AddUrlActions).expects("addRssUrl").returns(
+//             { "type": "something" }
+//         );
+//
+//         addUrlDom = TestUtils.renderIntoDocument(<Provider store={store}><AddUrl /></Provider>);
+//     });
+//
+//     it("should have help message", () => {
+//         console.log(addUrlDom.props.children);
+//     });
+// });
+//
