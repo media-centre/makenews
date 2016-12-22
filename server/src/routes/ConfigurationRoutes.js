7c56be28a60e3f5526e83958bbd68236226cd124
Murali Krishna <vmurali1444@gmail.com>
2016-12-22 12:48:17 +0530

    [#16-Mur/Dur] fix for added source highlighting

--- server/src/routes/ConfigurationRoutes.js
+++ server/src/routes/ConfigurationRoutes.js
@@ -3,12 +3,12 @@ import SourceConfigureRoute from "./helpers/SourceConfigureRoute";
 
 export default (app) => {
     app.get("/configure-sources", (request, response, next) => {
-        RouteLogger.instance().info("ConfiguredSourcesRoutes:: /configuredSources request received. url = %s", request.url);
+        RouteLogger.instance().info("ConfiguredSourcesRoutes:: /configure-sources request received. url = %s", request.url);
         new SourceConfigureRoute(request, response, next).fetchConfiguredSources();
     });
 
     app.put("/configure-sources", (request, response, next) => {
-        RouteLogger.instance().info("FacebookRoutes:: /facebook/configureSource request received. url = %s", request.url);
+        RouteLogger.instance().info("ConfiguredSourcesRoutes:: /configure-sources PUT request received. url = %s", request.url);
         new SourceConfigureRoute(request, response, next).addConfiguredSource();
     });
 };
