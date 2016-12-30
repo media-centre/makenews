1188b50753e021bba533a7b981d37254224d25fb
maharjun <maharjun@thoughtworks.com>
2016-12-30 16:24:23 +0530

    [19 Mah/Dug] -- finished client side code for twitter authentication

--- client/src/js/twitter/TwitterLogin.js
+++ client/src/js/twitter/TwitterLogin.js
@@ -1,6 +1,5 @@
 import TwitterRequestHandler from "../twitter/TwitterRequestHandler";
 import AppWindow from "../utils/AppWindow";
-import LoginPage from "../login/pages/LoginPage";
 import UserInfo from "../user/UserInfo";
 
 export default class TwitterLogin {
@@ -8,47 +7,31 @@ export default class TwitterLogin {
         return new TwitterLogin();
     }
 
-    constructor(authenticated) {
-        this.authenticated = authenticated;
-    }
-
-    static getInstance() {
-        return new Promise((resolve) => {
-            TwitterLogin.isAuthenticated().then(authenticated => {
-                resolve(new TwitterLogin(authenticated));
-            });
-        });
-    }
-
     login() {
         return new Promise((resolve, reject) => {
-            if(this.authenticated) {
-                resolve();
-            } else {
-                let twitterWindow = window.open("", "twitterWindow", "location=0,status=0,width=800,height=600");
-                this.requestToken().then((response) => {
-                    twitterWindow.location = response.authenticateUrl;
-                    let maxIterations = 150, iteration = 0;
-                    let timer = setInterval(() => { //eslint-disable-line max-nested-callbacks
-                        if(iteration > maxIterations) {
-                            clearInterval(timer);
+            let twitterWindow = window.open("", "twitterWindow", "location=0,status=0,width=800,height=600");
+            this.requestToken().then((response) => {
+                twitterWindow.location = response.authenticateUrl;
+                let maxIterations = 150, iteration = 0;
+                let timer = setInterval(() => { //eslint-disable-line max-nested-callbacks
+                    if(iteration > maxIterations) {
+                        clearInterval(timer);
+                        reject(false);
+                    }
+                    if(twitterWindow.closed) {
+                        clearInterval(timer);
+                        let appWindow = AppWindow.instance();
+                        if(appWindow.get("twitterLoginSucess")) {
+                            appWindow.set("twitterLoginSucess", false);
+                            this.authenticated = true;
+                            resolve(true);
+                        } else {
                             reject(false);
                         }
-                        if(twitterWindow.closed) {
-                            clearInterval(timer);
-                            let appWindow = AppWindow.instance();
-                            if(appWindow.get("twitterLoginSucess")) {
-                                appWindow.set("twitterLoginSucess", false);
-                                this.authenticated = true;
-                                resolve(true);
-                            } else {
-                                reject(false);
-                            }
-                        }
-                        iteration += 1;  // eslint-disable-line no-magic-numbers
-                    }, TwitterLogin.getWaitTime());
-                });
-            }
+                    }
+                    iteration += 1;  // eslint-disable-line no-magic-numbers
+                }, TwitterLogin.getWaitTime());
+            });
         });
     }
     static getWaitTime() {
@@ -60,7 +43,7 @@ export default class TwitterLogin {
         let appServerUrl = AppWindow.instance().get("serverUrl");
         let serverCallbackUrl = appServerUrl + "/twitter-oauth-callback";
         let clientCallbackUrl = appServerUrl + "/#/twitterSuccess";
-        return TwitterRequestHandler.requestToken(clientCallbackUrl, serverCallbackUrl, LoginPage.getUserName());
+        return TwitterRequestHandler.requestToken(clientCallbackUrl, serverCallbackUrl);
     }
 
     static isAuthenticated() {
