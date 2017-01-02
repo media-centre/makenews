e3cc26989fd9f290c552b5e3377913ac3b00fb47
Saisree <saisree.sree9@gmail.com>
2017-01-02 10:51:19 +0530

    [#17 - SSL/Swe] - getting username from the db for twitter access token and added test cases

---
+++ server/test/twitter/TwitterClientSpec.js
@@ -0,0 +1,8 @@
+import TwitterClient from "../../src/twitter/TwitterClient";
+
+xdescribe("TwitterClient", () => {
+    it("should fetch followers from the twitter user", () => {
+        let twitterClient = new TwitterClient();
+        //twitterClient.fetchFollowers()
+    });
+});
