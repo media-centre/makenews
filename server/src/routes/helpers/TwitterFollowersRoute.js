7fc0b84c166cd7f002d73eff1afc52bcae66d6e9
swethab <swethab@thoughtworks.com>
2017-01-03 18:40:01 +0530

    [17 - Swe/SSL] - added twitter configuration  pagination

--- server/src/routes/helpers/TwitterFollowersRoute.js
+++ server/src/routes/helpers/TwitterFollowersRoute.js
@@ -8,13 +8,20 @@ export default class TwitterFollowersRoute extends Route {
     constructor(request, response, next) {
         super(request, response, next);
         this.keyword = this.request.query.keyword;
+        this.page = this.request.query.page;
         this.authSession = this.request.cookies.AuthSession;
+    }
+
+    pageValue() {
+        let ONE = 1;
+        let page = Number.parseInt(this.page, 10);
+        return (Number.isInteger(page) && page >= ONE) ? page : ONE;
     }
 
     async handle() {    //eslint-disable-line consistent-return
         let twitterRequestHandler = TwitterRequestHandler.instance();
         try {
-            let data = await twitterRequestHandler.fetchFollowersRequest(this.authSession, this.keyword);
+            let data = await twitterRequestHandler.fetchFollowersRequest(this.authSession, this.keyword, this.pageValue());
             RouteLogger.instance().debug("TwitterFeedsRoute:: successfully fetched twitter feeds for key");
             this._handleSuccess(data);
         } catch(error){ //eslint-disable-line
