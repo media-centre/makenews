7538f799dd463dd356c843f46d65eb7b4fa4321a
swethab <swethab@thoughtworks.com>
2017-01-03 18:40:01 +0530

    [17 - Swe/SSL] - added twitter configuration  pagination

--- client/test/config/TwitterConfigureActionsSpec.js
+++ client/test/config/TwitterConfigureActionsSpec.js
@@ -3,6 +3,7 @@ import { assert } from "chai";
 import sinon from "sinon";
 import AjaxClient from "./../../src/js/utils/AjaxClient";
 import mockStore from "../helper/ActionHelper";
+import * as SearchResultsSetOperations from "../../src/js/utils/SearchResultsSetOperations";
 import { HAS_MORE_SOURCE_RESULTS, NO_MORE_SOURCE_RESULTS } from "./../../src/js/sourceConfig/actions/SourceConfigurationActions";
 
 describe("TwitterConfigureActions", () => {
@@ -39,7 +40,7 @@ describe("TwitterConfigureActions", () => {
         });
 
         it(`should dispatch ${TWITTER_GOT_SOURCE_RESULTS}, ${HAS_MORE_SOURCE_RESULTS} after getting sources`, (done) => {
-            let result = [{
+            let result = { "docs" : [{
                 "id": 8640348,
                 "name": "testUser",
                 "location": "andhra"
@@ -48,36 +49,12 @@ describe("TwitterConfigureActions", () => {
                     "id": 8640347,
                     "name": "testUser3",
                     "location": "andhrapradesh"
-                }];
+                }] };
             ajaxGetMock.returns(Promise.resolve(result));
 
             let gotTwitterSourcesActionObj = {
                 "type": TWITTER_GOT_SOURCE_RESULTS,
-                "sources": { "data": result, "paging": result.paging }
-            };
-
-            let store = mockStore({ "configuredSources": { "twitter": [] } }, [gotTwitterSourcesActionObj, { "type": HAS_MORE_SOURCE_RESULTS }], done);
-            store.dispatch(fetchTwitterSources(keyword));
-            ajaxGetMock.verify();
-        });
-
-        it.only(`should dispatch ${TWITTER_GOT_SOURCE_RESULTS}, ${HAS_MORE_SOURCE_RESULTS} after getting sources with added=true property`, (done) => {
-            let result =  [{
-                "id": 8640348,
-                "name": "testUser",
-                "location": "andhra"
-            },
-                {
-                    "id": 8640347,
-                    "name": "testUser3",
-                    "location": "andhrapradesh"
-                }];
-            ajaxGetMock.returns(Promise.resolve(result));
-
-            let gotWebSourcesActionObj = {
-                "type": TWITTER_GOT_SOURCE_RESULTS,
-                "sources": {
-                    "data":  [{
+                "sources": { "data": [{
                     "id": 8640348,
                     "name": "testUser",
                     "location": "andhra"
@@ -85,26 +62,62 @@ describe("TwitterConfigureActions", () => {
                     {
                         "id": 8640347,
                         "name": "testUser3",
-                        "location": "andhrapradesh",
-                        "added": true
-                    }],
-                    "paging": result.paging }
+                        "location": "andhrapradesh"
+                    }], "paging": result.paging }
+            };
+
+            let store = mockStore({ "configuredSources": { "twitter": [] } }, [gotTwitterSourcesActionObj, { "type": HAS_MORE_SOURCE_RESULTS }], done);
+            store.dispatch(fetchTwitterSources(keyword));
+            ajaxGetMock.verify();
+        });
+
+        it(`should dispatch ${TWITTER_GOT_SOURCE_RESULTS}, ${HAS_MORE_SOURCE_RESULTS} after getting sources with added=true property`, (done) => {
+            let result = { "docs": [{
+                "id": 8640348,
+                "name": "testUser",
+                "location": "andhra"
+            },
+                {
+                    "id": 8640347,
+                    "name": "testUser3",
+                    "location": "andhrapradesh"
+                }],
+                "paging": 6
+            };
+            ajaxGetMock.returns(Promise.resolve(result));
+
+            let gotWebSourcesActionObj = {
+                "type": TWITTER_GOT_SOURCE_RESULTS,
+                "sources": {
+                    "data": [{
+                        "id": 8640348,
+                        "name": "testUser",
+                        "location": "andhra"
+                    },
+                        {
+                            "id": 8640347,
+                            "name": "testUser3",
+                            "location": "andhrapradesh",
+                            "added": true
+                        }],
+                    "paging": result.paging
+                }
             };
 
             const getStore = { "configuredSources": {
                 "twitter": [{
                     "name": "testUser3",
-                    "id": 8640347
+                    "_id": 8640347
                 }]
             } };
-
             let store = mockStore(getStore, [gotWebSourcesActionObj, { "type": HAS_MORE_SOURCE_RESULTS }], done);
             store.dispatch(fetchTwitterSources(keyword));
             ajaxGetMock.verify();
         });
 
         it(`should dispatch ${NO_MORE_SOURCE_RESULTS} if sources are empty`, (done) => {
-            let result = { "docs": [], "paging": { "offset": "25" } };
+            let result = { "docs": [], "paging": {} };
+            sandbox.mock(SearchResultsSetOperations).expects("intersectionWith");
             ajaxGetMock.returns(Promise.resolve(result));
 
             let store = mockStore({}, [{ "type": NO_MORE_SOURCE_RESULTS }], done);
--- client/test/helper/ActionHelper.js
+++ client/test/helper/ActionHelper.js
@@ -25,10 +25,12 @@ export default function mockStore(getState, expectedActions, done, verify = func
                     expect(action).to.deep.equal(expectedAction);
                     if (done && !expectedActions.length && !failed) {
                         verify();
+                        console.log("done");
                         done();
                     }
                     return action;
                 } catch (error) {
+                        console.log("done with err");
                     done(error);
                     failed = true;
                 }
