e593ddc514ddfd19648a91228d4256489508b333
swethab <swethab@thoughtworks.com>
2017-01-19 14:05:25 +0530

    [33 Swe/Dur] - Added time check for fetch in different sources

--- server/src/util/Constants.js
+++ server/src/util/Constants.js
@@ -2,3 +2,4 @@
 export const fbSourceTypesToFetch = { "page": "page", "group": "group", "profile": "user" };
 export const sourceTypes = { "fb_page": "fb_page", "fb_profile": "fb_profile", "fb_group": "fb_group", "twitter": "twitter", "web": "web" };
 export const newsBoardSourceTypes = { "trending": ["web", "facebook", "twitter"], "web": ["web"], "facebook": ["facebook"], "twitter": ["twitter"] };
+export const fetchFeedsTimeInterval = { "web": 420000, "facebook": 360000, "twitter": 300000 };
