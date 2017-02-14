dcc7af73187728a698f3dd567d3c098a5ef24cd2
RaviBolla <ravisrs222@gmail.com>
2017-02-14 15:53:02 +0530

    [#31] reset messages in component unmount

--- client/src/js/config/components/AddUrl.jsx
+++ client/src/js/config/components/AddUrl.jsx
@@ -11,6 +11,10 @@ export class AddUrl extends Component {
         this.urlRegex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i; //eslint-disable-line max-len
     }
 
+    componentWillUnmount() {
+        this.props.dispatch(AddUrlActions.handleMessages());
+    }
+
     _addUrl() {
         let url = this.refs.url.value.trim();
         if (url.match(this.urlRegex)) {
