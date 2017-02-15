28796af9767ed797e00ac52721a8fd9482fafa59
Vankayalapati Nagadurga <nvankaya@thoughtworks.com>
2017-02-15 12:31:11 +0530

    [#20  Dur] - user profile - removed state from user profile and test cases

--- client/src/js/header/components/UserProfileTab.jsx
+++ client/src/js/header/components/UserProfileTab.jsx
@@ -3,26 +3,18 @@ import React, { Component } from "react";
 import UserProfile from "./UserProfile";
 
 export default class UserProfileTab extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { "show": false };
-    }
-
-    _toggleDropdown() {
-        this.setState({ "show": !this.state.show });
-    }
 
     render() {
         return (
-            <div className="user-profile" onClick={() => { this._toggleDropdown(); }} onMouseLeave={() => { this._toggleDropdown(); }}>
-                        <span className="user-profile__image">
-                            <img src="../../../images/userprofile-icon.png"/>
-                        </span>
-                        <span className="user-profile__name">User Profile</span>
-                        <span>
-                            <i className="fa fa-caret-down down-arrow" aria-hidden="true"/>
-                        </span>
-                <div className={this.state.show ? "user-profile__dropdown" : "user-profile__dropdown hide"}>
+            <div className="user-profile">
+                <span className="user-profile__image">
+                    <img src="../../../images/userprofile-icon.png"/>
+                </span>
+                <span className="user-profile__name">User Profile</span>
+                <span>
+                    <i className="fa fa-caret-down down-arrow" aria-hidden="true"/>
+                </span>
+                <div className="user-profile__dropdown">
                     <UserProfile />
                 </div>
             </div>
--- client/test/header/components/UserProfileTabSpec.js
+++ client/test/header/components/UserProfileTabSpec.js
@@ -2,26 +2,58 @@ import UserProfileTab from "./../../../src/js/header/components/UserProfileTab";
 import UserProfile from "./../../../src/js/header/components/UserProfile";
 import React from "react";
 import TestUtils from "react-addons-test-utils";
-import ReactDOM from "react-dom";
-import { expect, assert } from "chai";
+import { expect } from "chai";
 import { findAllWithType } from "react-shallow-testutils";
 
 describe("UserProfileTab", () => {
-    
-    it("should have toggle option", () => {
-        let userProfile = TestUtils.renderIntoDocument(<UserProfileTab />);
-        let userProfileDom = ReactDOM.findDOMNode(userProfile);
-        TestUtils.Simulate.click(userProfileDom.querySelector(".user-profile__image"));
-        assert.isTrue(userProfile.state.show);
-        TestUtils.Simulate.click(userProfileDom.querySelector(".user-profile__image"));
-        assert.isFalse(userProfile.state.show);
+    let renderer = null, result = null;
+
+    beforeEach("UserProfileTab", () => {
+        renderer = TestUtils.createRenderer();
+        renderer.render(<UserProfileTab />);
+        result = renderer.getRenderOutput();
     });
 
-    it("should have main header tabs component", () => {
-        let renderer = TestUtils.createRenderer();
-        renderer.render(<UserProfileTab />);
-        let result = renderer.getRenderOutput();
+    it("should have User Profile component", () => {
         let renderedSources = findAllWithType(result, UserProfile);
         expect(renderedSources).to.have.lengthOf(1); //eslint-disable-line no-magic-numbers
     });
+
+    it("should have user profile div", ()=> {
+        expect(result.type).to.be.equals("div");
+        expect(result.props.className).to.be.equals("user-profile");
+    });
+
+    it("should have image", ()=> {
+        let [img] = result.props.children;
+
+        expect(img.type).to.be.equals("span");
+        expect(img.props.className).to.be.equals("user-profile__image");
+        expect(img.props.children.type).to.be.equals("img");
+        expect(img.props.children.props.src).to.be.equals("../../../images/userprofile-icon.png");
+    });
+
+    it("should have name", ()=> {
+        let [, name] = result.props.children;
+
+        expect(name.type).to.be.equals("span");
+        expect(name.props.className).to.be.equals("user-profile__name");
+        expect(name.props.children).to.be.equals("User Profile");
+    });
+
+    it("should have down arrow", ()=> {
+        let [, , arrow] = result.props.children;
+
+        expect(arrow.type).to.be.equals("span");
+        expect(arrow.props.children.type).to.be.equals("i");
+        expect(arrow.props.children.props.className).to.be.equals("fa fa-caret-down down-arrow");
+    });
+
+    it("should have dropdown", ()=> {
+        let [, , , dropdown] = result.props.children;
+
+        expect(dropdown.type).to.be.equals("div");
+        expect(dropdown.props.className).to.be.equals("user-profile__dropdown");
+    });
+
 });
