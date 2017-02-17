9cc4ce461033c58b8301b28e0c5c9abf24d18cdd
Mahitha Arveti <marveti@thoughtworks.com>
2017-02-17 18:14:22 +0530

    Revert "[41 Swe/Mht] - server side implementation to get tweets for hashtags"
    This reverts commit 410ecbe8d7f7fc4f2f3724426b19437a5a946c62.

--- server/src/fetchAllFeeds/FetchFeedsFromAllSourcesRoutes.js
+++ server/src/fetchAllFeeds/FetchFeedsFromAllSourcesRoutes.js
@@ -1,15 +1,9 @@
 import FetchFeedsFromAllSources from "./FetchFeedsFromAllSources";
-import FetchFeedsFromHashTag from "./FetchFeedsFromHashtag";
 import RouteLogger from "../routes/RouteLogger";
 
 export default (app) => {
     app.post("/fetch-feeds", (request, response) => {
         RouteLogger.instance().info(`FetchFeedsFromAllSourcesRoutes:: /fetch-feeds request received. url = ${request.url}`);
         new FetchFeedsFromAllSources(request, response).fetchFeeds();
-    });
-
-    app.post("/fetch-hashtag", (request, response) => {
-        RouteLogger.instance().info(`FetchFeedsFromTwitterHashTags:: /fetch-feeds request received. url = ${request.url}`);
-        new FetchFeedsFromHashTag(request, response).process();
     });
 };
