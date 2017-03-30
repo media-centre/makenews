b7e27af4490e1e486e32e247bda34294cfe3eb29
Murali Krishna <vmurali1444@gmail.com>
2017-03-30 16:52:29 +0530

    [101 Mur/Dur] - client- actions for adding fb and twitter urls

--- client/src/js/config/actions/AddUrlActions.js
+++ client/src/js/config/actions/AddUrlActions.js
@@ -1,6 +1,8 @@
 import AjaxClient from "./../../utils/AjaxClient";
 import { WEB_ADD_SOURCE } from "./WebConfigureActions";
 import Toast from "../../utils/custom_templates/Toast";
+import { FACEBOOK_ADD_PAGE } from "./../actions/FacebookConfigureActions";
+import { TWITTER_ADD_SOURCE } from "./../actions/TwitterConfigureActions";
 
 export const RSS_ADD_URL_STATUS = "RSS_ADD_URL_STATUS";
 
@@ -29,5 +31,37 @@ export function handleMessages(added) {
     return {
         "type": RSS_ADD_URL_STATUS,
         "status": { added }
+    };
+}
+
+export function addFacebookPage(pageUrl) {
+    return async (dispatch) => {
+        try {
+            const ajax = AjaxClient.instance("/configure-facebook-page");
+            const headers = {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            };
+            const response = await ajax.put(headers, { pageUrl });
+            dispatch({ "type": FACEBOOK_ADD_PAGE, "sources": [response] });
+        } catch(err) {
+            Toast.show(err.message);
+        }
+    };
+}
+
+export function addTwitterHandle(twitterHandle) {
+    return async (dispatch) => {
+        try {
+            const ajax = AjaxClient.instance("/configure-twitter-handle");
+            const headers = {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            };
+            const response = await ajax.put(headers, { twitterHandle });
+            dispatch({ "type": TWITTER_ADD_SOURCE, "sources": [response] });
+        } catch(err) {
+            Toast.show(err.message);
+        }
     };
 }
--- client/test/config/AddUrlActionsSpec.js
+++ client/test/config/AddUrlActionsSpec.js
@@ -1,70 +1,140 @@
-import { addRssUrl, RSS_ADD_URL_STATUS } from "./../../src/js/config/actions/AddUrlActions";
+import { addRssUrl, addFacebookPage, addTwitterHandle, RSS_ADD_URL_STATUS } from "./../../src/js/config/actions/AddUrlActions";
+import { FACEBOOK_ADD_PAGE } from "./../../src/js/config/actions/FacebookConfigureActions";
+import { TWITTER_ADD_SOURCE } from "./../../src/js/config/actions/TwitterConfigureActions";
 import AjaxClient from "./../../src/js/utils/AjaxClient";
 import mockStore from "./../helper/ActionHelper";
 import sinon from "sinon";
+import Toast from "./../../src/js/utils/custom_templates/Toast";
 
 describe("AddUrl Actions", () => {
-    let message = null, url = null;
-    let ajaxClientInstance = null, ajaxClientMock = null, postMock = null;
     const headers = {
         "Accept": "application/json",
         "Content-Type": "application/json"
     };
 
-    beforeEach("AddUrl Actions", () => {
-        ajaxClientInstance = AjaxClient.instance("/add-url", true);
-        ajaxClientMock = sinon.mock(AjaxClient);
-        ajaxClientMock.expects("instance").returns(ajaxClientInstance);
-        postMock = sinon.mock(ajaxClientInstance);
+    describe("AddRssUrl", () => {
+        let message = null, url = null;
+        let ajaxClientInstance = null, ajaxClientMock = null, postMock = null;
+
+        beforeEach("AddRssUrl", () => {
+            ajaxClientInstance = AjaxClient.instance("/add-url", true);
+            ajaxClientMock = sinon.mock(AjaxClient);
+            ajaxClientMock.expects("instance").returns(ajaxClientInstance);
+            postMock = sinon.mock(ajaxClientInstance);
+        });
+
+        afterEach("AddRssUrl", () => {
+            ajaxClientMock.restore();
+            postMock.restore();
+        });
+
+        it("should return successful message", (done) => {
+            const name = "NewsClick Economy";
+            url = "http://newsclick.in/taxonomy/term/economy/feed";
+            const added = true;
+
+            postMock.expects("post").withArgs(headers, { url })
+                .returns(Promise.resolve({ url, name }));
+
+            const action = [{ "type": "WEB_ADD_SOURCE", "sources": [{ name, url }] },
+                { "type": RSS_ADD_URL_STATUS, "status": { added } }];
+            const store = mockStore([], action, done);
+            store.dispatch(addRssUrl(url, () => {
+                ajaxClientMock.verify();
+                postMock.verify();
+            }));
+        });
+
+        it("should return already exist message if url is exist in database", (done) => {
+            url = "http://newsclick.in/taxonomy/term/economy/feed";
+            const added = false;
+            postMock.expects("post").withArgs(headers, { "url": url })
+                .returns(Promise.reject({ message }));
+
+            const action = [{ "type": RSS_ADD_URL_STATUS, "status": { added } }];
+            const store = mockStore([], action, done);
+            store.dispatch(addRssUrl(url, () => {
+                ajaxClientMock.verify();
+                postMock.verify();
+            }));
+        });
+
+        it("should return invalid message if url is invalid", (done) => {
+            url = "http://x.com";
+            const added = false;
+
+            postMock.expects("post").withArgs(headers, { "url": url }).returns(Promise.reject({ message }));
+
+            const action = [{ "type": RSS_ADD_URL_STATUS, "status": { added } }];
+            const store = mockStore([], action, done);
+            store.dispatch(addRssUrl(url, () => {
+                ajaxClientMock.verify();
+                postMock.verify();
+            }));
+        });
     });
 
-    afterEach("AddUrl Actions", () => {
-        ajaxClientMock.restore();
-        postMock.restore();
+    describe("AddFacebookPage", () => {
+        let ajaxClientInstance = null;
+        const sandbox = sinon.sandbox.create();
+
+        afterEach("AddFacebookPage", () => {
+            sandbox.restore();
+        });
+
+        it("should return successful message", (done) => {
+            const pageUrl = "https://www.facebook.com/test";
+            ajaxClientInstance = AjaxClient.instance("/configure-facebook-page");
+            sandbox.stub(AjaxClient, "instance").returns(ajaxClientInstance);
+            sandbox.mock(ajaxClientInstance).expects("put")
+                .withExactArgs(headers, { pageUrl }).returns(Promise.resolve({ "id": 1, "name": "page name" }));
+            const action = [{ "type": FACEBOOK_ADD_PAGE, "sources": [{ "id": 1, "name": "page name" }] }];
+            const store = mockStore([], action, done);
+            store.dispatch(addFacebookPage(pageUrl));
+        });
+
+        it("should should show toast message if an error occurs while adding the page", async() => {
+            const pageUrl = "test";
+            ajaxClientInstance = AjaxClient.instance("/configure-facebook-page");
+            sandbox.stub(AjaxClient, "instance").returns(ajaxClientInstance);
+            const toastMock = sandbox.mock(Toast).expects("show").withExactArgs("unable to add the url");
+            sandbox.mock(ajaxClientInstance).expects("put").withExactArgs(headers, { pageUrl })
+                .returns(Promise.reject({ "message": "unable to add the url" }));
+            
+            await addFacebookPage(pageUrl)();
+            toastMock.verify();
+        });
     });
 
-    it("should return successful message", (done) => {
-        const name = "NewsClick Economy";
-        url = "http://newsclick.in/taxonomy/term/economy/feed";
-        const added = true;
+    describe("AddTwitterHandle", () => {
+        let ajaxClientInstance = null;
+        const sandbox = sinon.sandbox.create();
 
-        postMock.expects("post").withArgs(headers, { url })
-            .returns(Promise.resolve({ url, name }));
+        afterEach("AddTwitterHandle", () => {
+            sandbox.restore();
+        });
 
-        const action = [{ "type": "WEB_ADD_SOURCE", "sources": [{ name, url }] },
-            { "type": RSS_ADD_URL_STATUS, "status": { added } }];
-        const store = mockStore([], action, done);
-        store.dispatch(addRssUrl(url, () => {
-            ajaxClientMock.verify();
-            postMock.verify();
-        }));
-    });
+        it("should return successful message", (done) => {
+            const twitterHandle = "@test";
+            ajaxClientInstance = AjaxClient.instance("/configure-twitter-handle");
+            sandbox.stub(AjaxClient, "instance").returns(ajaxClientInstance);
+            sandbox.mock(ajaxClientInstance).expects("put")
+                .withExactArgs(headers, { twitterHandle }).returns(Promise.resolve({ "id": 1, "name": "handle name" }));
+            const action = [{ "type": TWITTER_ADD_SOURCE, "sources": [{ "id": 1, "name": "handle name" }] }];
+            const store = mockStore([], action, done);
+            store.dispatch(addTwitterHandle(twitterHandle));
+        });
 
-    it("should return already exist message if url is exist in database", (done) => {
-        url = "http://newsclick.in/taxonomy/term/economy/feed";
-        const added = false;
-        postMock.expects("post").withArgs(headers, { "url": url })
-            .returns(Promise.reject({ message }));
+        it("should should show toast message if an error occurs while adding the handle", async () => {
+            const twitterHandle = "blaw";
+            ajaxClientInstance = AjaxClient.instance("/configure-twitter-handle");
+            sandbox.stub(AjaxClient, "instance").returns(ajaxClientInstance);
+            const toastMock = sandbox.mock(Toast).expects("show").withExactArgs("unable to add the url");
+            sandbox.mock(ajaxClientInstance).expects("put").withExactArgs(headers, { twitterHandle })
+                .returns(Promise.reject({ "message": "unable to add the url" }));
 
-        const action = [{ "type": RSS_ADD_URL_STATUS, "status": { added } }];
-        const store = mockStore([], action, done);
-        store.dispatch(addRssUrl(url, () => {
-            ajaxClientMock.verify();
-            postMock.verify();
-        }));
-    });
-
-    it("should return invalid message if url is invalid", (done) => {
-        url = "http://x.com";
-        const added = false;
-
-        postMock.expects("post").withArgs(headers, { "url": url }).returns(Promise.reject({ message }));
-
-        const action = [{ "type": RSS_ADD_URL_STATUS, "status": { added } }];
-        const store = mockStore([], action, done);
-        store.dispatch(addRssUrl(url, () => {
-            ajaxClientMock.verify();
-            postMock.verify();
-        }));
+            await (addTwitterHandle(twitterHandle)());
+            toastMock.verify();
+        });
     });
 });
