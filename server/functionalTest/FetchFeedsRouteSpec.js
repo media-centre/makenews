38b895619fdc04d1e27504adf518bafdf08e1ce6
Saisree <saisree.sree9@gmail.com>
2017-04-05 18:24:51 +0530

    [SSL/Swe] - functional tests - added Facebook, Twitter, Web Ruotes

--- server/functionalTest/FetchFeedsRouteSpec.js
+++ server/functionalTest/FetchFeedsRouteSpec.js
@@ -2,16 +2,29 @@ import request from "supertest";
 import { assert } from "chai";
 import ApplicationConfig from "../src/config/ApplicationConfig";
 import CouchClient from "../src/CouchClient";
+import CryptUtil from "../src/util/CryptUtil";
 
 describe("FetchFeedsRoute", () => {
     let accessToken = null, applicationConfig = null, serverIp = null;
+    let couchClient = null, sourceDocument = null;
+
     before("FetchFeedsRoute", (done) => {
         applicationConfig = new ApplicationConfig();
         serverIp = applicationConfig.serverIpAddress() + ":" + applicationConfig.serverPort();
-        const sourceDocument = {
-            "_id": "118170f0e2e427469e66f5f2740009e7",
-            "docType": "source",
-            "sourceType": "fb_page"
+        sourceDocument = {
+            "docs": [{
+                "_id": "118170f0e2e427469e66f5f2740009e7",
+                "docType": "source",
+                "sourceType": "fb_page"
+            }, {
+                "_id": "128170f0e2e427469e66f5f2740009e7",
+                "docType": "source",
+                "sourceType": "twitter"
+            }, {
+                "_id": "http://localhost:3000/https://www.thehindu.com",
+                "docType": "source",
+                "sourceType": "web"
+            }]
         };
 
         const user = { "username": "test", "password": "test" };
@@ -20,8 +33,10 @@ describe("FetchFeedsRoute", () => {
             .send(user)
             .then((resp) => {
                 accessToken = resp.headers["set-cookie"].pop().split(";")[0].split("=")[1];  //eslint-disable-line no-magic-numbers
-                const couchClient = new CouchClient(accessToken, "db_ad71148c79f21ab9eec51ea5c7dd2b668792f7c0d3534ae66b22f71c61523fb3");
-                couchClient.updateDocument(sourceDocument).then(()=> {
+                couchClient = new CouchClient(accessToken, CryptUtil.dbNameHash("test"));
+                couchClient.saveBulkDocuments(sourceDocument).then(()=> {
+                    done();
+                }).catch(err => { //eslint-disable-line
                     done();
                 });
             }).catch(error => {
@@ -30,14 +45,31 @@ describe("FetchFeedsRoute", () => {
 
     });
 
+    after("FetchFeedsRoute", (done) => {
+        sourceDocument.docs.forEach((doc, index) => {
+            couchClient.deleteDocument(encodeURIComponent(doc._id))
+                .then(() => {
+                    if(index === (sourceDocument.docs.length - 1)) { //eslint-disable-line no-magic-numbers
+                        done();
+                    }
+                })
+                .catch(err => {
+                    err.msg = `Unable to delete doc ${doc._id}`;
+                    done(err);
+                });
+        });
+    });
+
+
     describe("/fetch-feeds", () => {
-        it("should fetch feeds from all sources", () => {
+        it("should fetch feeds from all sources", (done) => {
             request(serverIp)
                 .post("/fetch-feeds")
-                .set("Cookie", accessToken)
+                .set("Cookie", `AuthSession=${accessToken}`)
                 .end((err, res) => { //eslint-disable-line handle-callback-err
                     assert.equal(res.statusCode, 200); //eslint-disable-line no-magic-numbers
                     assert.deepEqual(res.body, { "status": true });
+                    done();
                 });
         });
     });
--- testserver/FetchFeedsRoutes.js
+++ testserver/FetchFeedsRoutes.js
@@ -3,7 +3,7 @@
 
 function fetchFeedsRoutes(app) {
     app.get("/https://www.facebook.com/118170f0e2e427469e66f5f2740009e7/posts", (request, response, next) => {
-        console.log("/https://www.facebook.com/sourceId/fb_page request received");
+        console.log("/https://www.facebook.com/118170f0e2e427469e66f5f2740009e7/fb_page request received");
         response.status(200);
         response.json({
             "data": [{ "message": "test news 1", "id": "163974433696568_957858557641481", "from": { "name": "some" } },
@@ -13,6 +13,36 @@ function fetchFeedsRoutes(app) {
                 "next": "https://graph.facebook.com/v2.8/1608617579371619/posts?limit=25&until=1475253123&format=json"
             }
         });
+        next();
+    });
+
+
+    app.get("/https://api.twitter.com/statuses/user_timeline.json", (request, response, next) => {
+        console.log("/https://api.twitter.com/statuses/user_timeline.json request received");
+        response.status(200);
+        response.json([{
+            "id": 835103042471096320,
+            "id_str": "835103042471096320",
+            "created_at": "fri dec 09 07:24:44 +0000 2016",
+            "text": "just posted a photo https://t.co/7x7kvw9plf",
+            "user": {
+                "name": "user1"
+            },
+            "entities": {
+                "hashtags": []
+            }
+        }]);
+        next();
+    });
+
+    app.get("/https://www.thehindu.com", (request, response, next) => {
+        console.log("/https://www.thehindu.com request received");
+        response.status(200);
+        response.set("Content-Type", "text/xml");
+        response.send("<rss version=\"2.0\"><channel><item>" +
+            "<title>sample1</title><description>news cricket</description></item><item>" +
+            "<title>sample2</title><description>news politics</description></item>" +
+            "</channel></rss>");
         next();
     });
 }
