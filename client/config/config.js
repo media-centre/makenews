0bee5e5abc7866e7a2012e40e8e5505ae32f718e
RaviBolla <ravisrs222@gmail.com>
2017-05-02 14:23:23 +0530

    [Rav] removing unused configuration

--- client/config/config.js
+++ client/config/config.js
@@ -7,6 +7,5 @@ window.mediaCenter.serverUrl = "http://localhost:5000";
 window.mediaCenter.facebookAppId = "158896271244772";
 window.mediaCenter.maxSurfFeedsLifeInDays = 30;
 window.mediaCenter.autoRefreshSurfFeedsInterval = 300000;
-window.mediaCenter.numberOfDaysToBackUp = 2;
 window.mediaCenter.dbSessionInterval = 600000;
 window.mediaCenter.storyAutoSaveTimeInterval = 300000;
