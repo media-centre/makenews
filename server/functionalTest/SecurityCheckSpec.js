fa927bcda3690f85d31cfc8acafba1e2d516f9dc
RaviBolla <ravisrs222@gmail.com>
2017-05-08 13:17:57 +0530

    [Rav] security settings

--- server/functionalTest/SecurityCheckSpec.js
+++ server/functionalTest/SecurityCheckSpec.js
@@ -1,5 +1,3 @@
-/* eslint max-nested-callbacks: [2, 5] react/jsx-wrap-multilines:0 */
-/* eslint handle-callback-err: 0 no-magic-numbers:0  */
 import request from "supertest";
 import { expect, assert } from "chai";
 import argv from "yargs";
@@ -11,7 +9,7 @@ describe("SecurityCheckSpec", () => {
         it("response to / should not have x-powered-by ", (done) => {
             request(config[env].serverIpAddress + ":" + config[env].serverPort)
                 .post("/")
-                .end((err, res) => {
+                .end((err, res) => { //eslint-disable-line handle-callback-err
                     expect(res.headers).to.not.have.property("x-powered-by");
                     done();
                 });
@@ -20,7 +18,7 @@ describe("SecurityCheckSpec", () => {
         it("response to /rss-feeds should not have x-powered-by ", (done) => {
             request(config[env].serverIpAddress + ":" + config[env].serverPort)
                 .post("/rss-feeds")
-                .end((err, res) => {
+                .end((err, res) => { //eslint-disable-line handle-callback-err
                     expect(res.headers).to.not.have.property("x-powered-by");
                     done();
                 });
@@ -28,7 +26,7 @@ describe("SecurityCheckSpec", () => {
         it("response to /login should not have x-powered-by ", (done) => {
             request(config[env].serverIpAddress + ":" + config[env].serverPort)
                 .post("/login")
-                .end((err, res) => {
+                .end((err, res) => { //eslint-disable-line handle-callback-err
                     expect(res.headers).to.not.have.property("x-powered-by");
                     done();
                 });
@@ -38,99 +36,18 @@ describe("SecurityCheckSpec", () => {
             request(config[env].serverIpAddress + ":" + config[env].serverPort)
                 .post("/login")
                 .send(user)
-                .end((err, res) => {
+                .end((err, res) => { //eslint-disable-line handle-callback-err
                     expect(res.headers).to.not.have.property("x-powered-by");
                     done();
                 });
         });
     });
-    describe("SecurityCheck for x-content-type-options:nosniff", () => {
-        it("response to / should  have x-content-type-options:nosniff ", (done) => {
-            request(config[env].serverIpAddress + ":" + config[env].serverPort)
-                .post("/")
-                .end((err, res) => {
-                    expect(res.headers).to.have.property("x-content-type-options");
-                    assert.equal(res.headers["x-content-type-options"], "nosniff");
-                    done();
-                });
-        });
-        it("response to /rss-feeds should  have x-content-type-options:nosniff ", (done) => {
-            request(config[env].serverIpAddress + ":" + config[env].serverPort)
-                .post("/rss-feeds")
-                .end((err, res) => {
-                    expect(res.headers).to.have.property("x-content-type-options");
-                    assert.equal(res.headers["x-content-type-options"], "nosniff");
-                    done();
-                });
-        });
-        it("response to /login should  have x-content-type-options:nosniff without login ", (done) => {
-            request(config[env].serverIpAddress + ":" + config[env].serverPort)
-                .post("/login")
-                .end((err, res) => {
-                    expect(res.headers).to.have.property("x-content-type-options");
-                    assert.equal(res.headers["x-content-type-options"], "nosniff");
-                    done();
-                });
-        });
-        it("response to /login should   have x-content-type-options:nosniff with login ", (done) => {
-            let user = { "username": "test", "password": "test" };
-            request(config[env].serverIpAddress + ":" + config[env].serverPort)
-                .post("/login")
-                .send(user)
-                .end((err, res) => {
-                    expect(res.headers).to.have.property("x-content-type-options");
-                    assert.equal(res.headers["x-content-type-options"], "nosniff");
-                    done();
-                });
-        });
-    });
 
-    describe("SecurityCheck for x-frame-options:DENY", () => {
-
-        it("response to / should  have x-frame-options:DENY ", (done) => {
-            request(config[env].serverIpAddress + ":" + config[env].serverPort)
-                .post("/")
-                .end((err, res) => {
-                    expect(res.headers).to.have.property("x-frame-options");
-                    assert.equal(res.headers["x-frame-options"], "DENY");
-                    done();
-                });
-        });
-        it("response to /rss-feeds should  have x-frame-options:DENY ", (done) => {
-            request(config[env].serverIpAddress + ":" + config[env].serverPort)
-                .post("/rss-feeds")
-                .end((err, res) => {
-                    expect(res.headers).to.have.property("x-frame-options");
-                    assert.equal(res.headers["x-frame-options"], "DENY");
-                    done();
-                });
-        });
-        it("response to /login should  have x-frame-options:DENY without login ", (done) => {
-            request(config[env].serverIpAddress + ":" + config[env].serverPort)
-                .post("/login")
-                .end((err, res) => {
-                    expect(res.headers).to.have.property("x-frame-options");
-                    assert.equal(res.headers["x-frame-options"], "DENY");
-                    done();
-                });
-        });
-        it("response to /login should  have x-frame-options:DENY with login ", (done) => {
-            let user = { "username": "test", "password": "test" };
-            request(config[env].serverIpAddress + ":" + config[env].serverPort)
-                .post("/login")
-                .send(user)
-                .end((err, res) => {
-                    expect(res.headers).to.have.property("x-frame-options");
-                    assert.equal(res.headers["x-frame-options"], "DENY");
-                    done();
-                });
-        });
-    });
     describe("SecurityCheck for x-xss-protection 1; mode=block", () => {
         it("response to / should  havex-xss-protection 1; mode=block ", (done) => {
             request(config[env].serverIpAddress + ":" + config[env].serverPort)
                 .post("/")
-                .end((err, res) => {
+                .end((err, res) => { //eslint-disable-line handle-callback-err
                     expect(res.headers).to.have.property("x-xss-protection");
                     assert.equal(res.headers["x-xss-protection"], "1; mode=block");
                     done();
@@ -139,7 +56,7 @@ describe("SecurityCheckSpec", () => {
         it("response to /rss-feeds should  have x-xss-protection 1; mode=block ", (done) => {
             request(config[env].serverIpAddress + ":" + config[env].serverPort)
                 .post("/rss-feeds")
-                .end((err, res) => {
+                .end((err, res) => { //eslint-disable-line handle-callback-err
                     expect(res.headers).to.have.property("x-xss-protection");
                     assert.equal(res.headers["x-xss-protection"], "1; mode=block");
                     done();
@@ -148,7 +65,7 @@ describe("SecurityCheckSpec", () => {
         it("response to /login should  have x-xss-protection 1; mode=block  without login ", (done) => {
             request(config[env].serverIpAddress + ":" + config[env].serverPort)
                 .post("/login")
-                .end((err, res) => {
+                .end((err, res) => { //eslint-disable-line handle-callback-err
                     expect(res.headers).to.have.property("x-xss-protection");
                     assert.equal(res.headers["x-xss-protection"], "1; mode=block");
                     done();
@@ -159,7 +76,7 @@ describe("SecurityCheckSpec", () => {
             request(config[env].serverIpAddress + ":" + config[env].serverPort)
                 .post("/login")
                 .send(user)
-                .end((err, res) => {
+                .end((err, res) => { //eslint-disable-line handle-callback-err
                     expect(res.headers).to.have.property("x-xss-protection");
                     assert.equal(res.headers["x-xss-protection"], "1; mode=block");
                     done();
