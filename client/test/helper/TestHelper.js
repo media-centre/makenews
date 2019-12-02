5e97618b591e48c5e4577ee0ee2a78fe13e7e8ab
Murali Krishna <vmurali1444@gmail.com>
2019-12-02 12:04:21 +0100

    #148 | [Murali] | add appConfig in test-setup to avoid reading actual config file during unit testing

--- client/test/helper/TestHelper.js
+++ client/test/helper/TestHelper.js
@@ -1,4 +1,7 @@
 import { JSDOM } from "jsdom";
+import sinon from "sinon";
+import EnvironmentFileLoader from "../../../common/src/environment/EnvironmentFileLoader";
+import EnvironmentReader from "../../../common/src/environment/EnvironmentReader";
 
 const { window } = new JSDOM("<!doctype html><html><body></body></html>");
 global.window = window;
@@ -17,4 +20,35 @@ global.sessionStorage = {
         delete this.item[key];
     }
 };
+
+const testConfig = {
+    "default": {
+        "serverIpAddress": "127.0.0.1",
+        "serverPort": 5000,
+        "couchDbUrl": "http://127.0.0.1:5984",
+        "searchEngineUrl": "http://127.0.0.1:5985/local",
+        "userDbPrefix": "db_",
+        "adminDetails": {
+            "username": "admin",
+            "password": "admin",
+            "db": "common"
+        },
+        "facebook": {
+            "url": "http://localhost:3000/https://www.facebook.com",
+            "appSecretKey": "asdf",
+            "appId": "asdf",
+            "timeOut": 4000,
+            "limit": 500
+        },
+        "twitter": {
+            "url": "https://api.twitter.com/1.1",
+            "authenticateUrl": "https://api.twitter.com/oauth/authenticate",
+            "consumerKey": "",
+            "consumerSecret": ""
+        }
+    }
+};
+
+sinon.stub(EnvironmentFileLoader, "instance").returns(new EnvironmentReader(testConfig, "default"));
+
 global.XMLHttpRequest = window.XMLHttpRequest;
