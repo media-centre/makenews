1438854f589ffc23cf8ff677a66732b3144427f8
akrstova <krstova.alisa@gmail.com>
2020-02-03 18:16:21 +0100

    Minor change

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,37 @@
 /*eslint no-console:0, no-path-concat:0 */
-var parameters = require("./config/parameters");
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var babelify = require("babelify");
-var babel = require("gulp-babel");
-var mocha = require("gulp-mocha");
-var eslint = require("gulp-eslint");
-var exec = require("child_process").exec;
-var del = require("del");
-var cssnano = require("gulp-cssnano");
-var environments = require("gulp-environments");
-var browserify = require("browserify");
-var source = require("vinyl-source-stream");
-var uglify = require("gulp-uglify");
-var buffer = require("vinyl-buffer");
-var envify = require("gulp-envify");
-var cordova = require("cordova-lib").cordova.raw;
-var livereload = require('gulp-livereload');
+const parameters = require("./config/parameters");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const babelify = require("babelify");
+const babel = require("gulp-babel");
+const mocha = require("gulp-mocha");
+const eslint = require("gulp-eslint");
+const exec = require("child_process").exec;
+const del = require("del");
+const cssnano = require("gulp-cssnano");
+const environments = require("gulp-environments");
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const uglify = require("gulp-uglify");
+const buffer = require("vinyl-buffer");
+const envify = require("gulp-envify");
+const cordova = require("cordova-lib").cordova.raw;
+const livereload = require("gulp-livereload");
 
 
-var development = environments.development; //eslint-disable-line no-unused-vars
-var production = environments.production;
+const development = environments.development; //eslint-disable-line no-unused-vars
+const production = environments.production;
 
 function clean(path) {
     return del(path);
 }
-gulp.task("mobile:remove-directory", function() {
-    var files = "." + parameters.mobile.mobilePath;
+
+gulp.task("mobile:remove-directory", function () {
+    const files = "." + parameters.mobile.mobilePath;
     return clean(files);
 });
 
-gulp.task("mobile:init", gulp.series("mobile:remove-directory", function(cb) {
+gulp.task("mobile:init", gulp.series("mobile:remove-directory", function (cb) {
     process.chdir(__dirname + "/dist/");
     exec("cordova create mobile com.makenews.android MakeNews", (err, stdout, stderr) => {
         console.log(stdout);
@@ -40,7 +41,7 @@ gulp.task("mobile:init", gulp.series("mobile:remove-directory", function(cb) {
     });
 }));
 
-gulp.task("mobile:create", function(cb) {
+gulp.task("mobile:create", function (cb) {
     process.chdir(__dirname + parameters.mobile.mobilePath);
     exec("cordova platform add android ", (err, stdout, stderr) => {
         console.log(stdout);
@@ -49,48 +50,52 @@ gulp.task("mobile:create", function(cb) {
     });
 });
 
-gulp.task("mobile:clean-files", function() {
-    var files = parameters.mobile.cordovaPath + "/*";
+gulp.task("mobile:clean-files", function () {
+    const files = parameters.mobile.cordovaPath + "/*";
     return clean(files);
 });
 
-gulp.task("mobile:copy-files", gulp.series("mobile:clean-files", function() {
+gulp.task("mobile:copy-files", gulp.series("mobile:clean-files", function () {
     return gulp.src([parameters.mobile.appPath + "/**/*"]).pipe(gulp.dest(parameters.mobile.cordovaPath));
 }));
 
-gulp.task("mobile:build", gulp.series("mobile:copy-files", function(cb) {
+gulp.task("mobile:build", gulp.series("mobile:copy-files", function (cb) {
     process.chdir(__dirname + parameters.mobile.mobilePath);
-    cordova.build().then(function() {
+    cordova.build().then(function () {
         process.chdir("../");
         cb();
-    }).catch(function(err) {
+    }).catch(function (err) {
         console.log(err);
     });
 }));
 
-gulp.task("client:scss", function() {
+gulp.task("client:scss", function () {
     return gulp.src([parameters.client.scssSrcPath + "/app.scss"])
-          .pipe(sass({
-              "sourceComments": true
-          }))
-          .pipe(production(cssnano()))
-          .pipe(gulp.dest(parameters.client.distFolder));
+        .pipe(sass({
+            "sourceComments": true
+        }))
+        .pipe(production(cssnano()))
+        .pipe(gulp.dest(parameters.client.distFolder));
 });
 
 
-gulp.task("client:images", function() {
+gulp.task("client:images", function () {
     return gulp.src(parameters.client.imgSrcPath + "/**/*.*")
-          .pipe(gulp.dest(parameters.client.distFolder + "/images"));
+        .pipe(gulp.dest(parameters.client.distFolder + "/images"));
 });
 
-gulp.task("client:build-sources", function() {
+gulp.task("client:build-sources", function () {
     gulp.src(parameters.client.clientAppPath + "/config/*.js")
         .pipe(gulp.dest(parameters.client.distFolder + "/config"));
 
-    return browserify({ "entries": parameters.client.srcPath + "/index.jsx", "extensions": [".jsx", ".js"], "debug": development() })
+    return browserify({
+        "entries": parameters.client.srcPath + "/index.jsx",
+        "extensions": [".jsx", ".js"],
+        "debug": development()
+    })
         .transform(babelify)
         .bundle()
-        .on("error", function(err) {
+        .on("error", function (err) {
             console.log("Error : " + err.message);
         })
         .pipe(source("app-min.js"))
@@ -101,9 +106,9 @@ gulp.task("client:build-sources", function() {
 });
 
 
-gulp.task("client:copy-resources", function() {
+gulp.task("client:copy-resources", function () {
     gulp.src(parameters.client.clientAppPath + "/index.html")
-    .pipe(gulp.dest(parameters.client.distFolder));
+        .pipe(gulp.dest(parameters.client.distFolder));
 
     gulp.src(parameters.client.imgSrcPath + "/**/*.*")
         .pipe(gulp.dest(parameters.client.distFolder + "/images"));
@@ -112,18 +117,18 @@ gulp.task("client:copy-resources", function() {
         .pipe(gulp.dest(parameters.client.distFolder + "/fonts"));
 });
 
-gulp.task("client:clean", function() {
+gulp.task("client:clean", function () {
     del(parameters.client.distFolder);
 });
 
-gulp.task("client:test", function() {
-    return gulp.src([parameters.client.testPath + "**/**/*.jsx", parameters.client.testPath + "**/**/*.js"], { "read": false })
-        .pipe(mocha({ "require": ["babel-core/register"] }));
+gulp.task("client:test", function () {
+    return gulp.src([parameters.client.testPath + "**/**/*.jsx", parameters.client.testPath + "**/**/*.js"], {"read": false})
+        .pipe(mocha({"require": ["babel-core/register"]}));
 });
 
 gulp.task("client:build", gulp.series("client:copy-resources", "client:build-sources", "client:scss"));
 
-gulp.task("client:watch", function() {
+gulp.task("client:watch", function () {
     livereload.listen();
     this.cssFilesPath = parameters.client.scssSrcPath + "/**/*.scss";
     this.copyFilesPath = [parameters.client.imgSrcPath + "/**/*.*", parameters.client.fontsPath + "/**/*.*"];
@@ -137,14 +142,14 @@ gulp.task("client:watch", function() {
     gulp.watch(this.appPath, gulp.series(["client:copy-resources"]));
 });
 
-gulp.task("client:src-eslint", function() {
+gulp.task("client:src-eslint", function () {
     return gulp.src([parameters.client.srcPath + "/**/*.jsx", parameters.client.srcPath + "/**/*.js"])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 
-gulp.task("client:test-eslint", function() {
+gulp.task("client:test-eslint", function () {
     return gulp.src([parameters.client.testPath + "/**/*.jsx", parameters.client.testPath + "/**/*.js"])
         .pipe(eslint())
         .pipe(eslint.format())
@@ -164,16 +169,16 @@ gulp.task("client:test-coverage", (cb) => {
 
 //-------------------------------- functional tests --------------------------------------
 
-gulp.task("functional:eslint", function() {
+gulp.task("functional:eslint", function () {
     return gulp.src([parameters.functional.serverSpecPath + "/**/*.js", parameters.functional.testServerPath + "/*.js"])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 
-gulp.task("functional:test", function() {
-    return gulp.src(parameters.functional.serverSpecPath + "**/**/*.js", { "read": false })
-        .pipe(mocha({ "require": ["babel-core/register"], "timeout": 3000 }));
+gulp.task("functional:test", function () {
+    return gulp.src(parameters.functional.serverSpecPath + "**/**/*.js", {"read": false})
+        .pipe(mocha({"require": ["babel-core/register"], "timeout": 3000}));
 });
 
 gulp.task("functional:test:watch", () => {
@@ -181,15 +186,15 @@ gulp.task("functional:test:watch", () => {
 });
 
 // -------------------------------common tasks -------------------------------------------
-gulp.task("common:copy-js", function() {
+gulp.task("common:copy-js", function () {
     return gulp.src(parameters.common.srcPath + "/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest(parameters.common.distFolder + "/src"));
 });
 
-gulp.task("common:test", function() {
-    return gulp.src(parameters.common.testPath + "/**/**/*.js", { "read": false })
-        .pipe(mocha({ "require": ["babel-core/register"] }));
+gulp.task("common:test", function () {
+    return gulp.src(parameters.common.testPath + "/**/**/*.js", {"read": false})
+        .pipe(mocha({"require": ["babel-core/register"]}));
 });
 
 gulp.task("common:watch", () => {
@@ -197,14 +202,14 @@ gulp.task("common:watch", () => {
     gulp.watch(`${parameters.common.testPath}/**/**/*.js`, gulp.series(["common:test"]));
 });
 
-gulp.task("common:src-eslint", function() {
+gulp.task("common:src-eslint", function () {
     return gulp.src([parameters.common.srcPath + "/**/*.js"])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 
-gulp.task("common:test-eslint", function() {
+gulp.task("common:test-eslint", function () {
     return gulp.src([parameters.common.testPath + "**/*.js"])
         .pipe(eslint())
         .pipe(eslint.format())
@@ -212,7 +217,7 @@ gulp.task("common:test-eslint", function() {
 });
 
 gulp.task("common:build", gulp.series("common:copy-js"));
-gulp.task("common:clean", function() {
+gulp.task("common:clean", function () {
     del(parameters.common.distFolder);
 });
 
@@ -228,10 +233,10 @@ gulp.task("common:test-coverage", (cb) => {
 });
 
 // -------------------------------server tasks -------------------------------------------
-gulp.task("server:copy-js", function() {
+gulp.task("server:copy-js", function () {
     gulp.src([parameters.server.serverAppPath + "/src/**/*.js"])
-    .pipe(babel())
-    .pipe(gulp.dest(parameters.server.distFolder + "/src"));
+        .pipe(babel())
+        .pipe(gulp.dest(parameters.server.distFolder + "/src"));
 
     gulp.src(parameters.server.serverAppPath + "/config/**/*.json")
         .pipe(gulp.dest(parameters.server.distFolder + "/config"));
@@ -240,30 +245,30 @@ gulp.task("server:copy-js", function() {
         .pipe(gulp.dest(parameters.server.distFolder + "/src"));
 
     gulp.src("./" + parameters.server.serverJsFile)
-    .pipe(babel())
-    .pipe(gulp.dest(parameters.server.distServerJsFolder));
+        .pipe(babel())
+        .pipe(gulp.dest(parameters.server.distServerJsFolder));
 
     gulp.src("./create_user.sh")
         .pipe(gulp.dest(parameters.server.distServerJsFolder));
 
     return gulp.src("./" + parameters.server.packageJsonFile)
-    .pipe(gulp.dest(parameters.server.distServerJsFolder));
+        .pipe(gulp.dest(parameters.server.distServerJsFolder));
 
 });
 
-gulp.task("server:clean", function() {
+gulp.task("server:clean", function () {
     del(parameters.server.distFolder);
     return del(parameters.server.distServerJsFolder + "/" + parameters.server.serverJsFile);
 });
 
-gulp.task("server:test", function() {
-    return gulp.src(parameters.server.testPath + "**/**/*.js", { "read": false })
-        .pipe(mocha({ "require": ["babel-core/register"] }));
+gulp.task("server:test", function () {
+    return gulp.src(parameters.server.testPath + "**/**/*.js", {"read": false})
+        .pipe(mocha({"require": ["babel-core/register"]}));
 });
 
 gulp.task("server:build", gulp.series("server:copy-js"));
 
-gulp.task("server:watch", function() {
+gulp.task("server:watch", function () {
     this.srcPath = parameters.server.srcPath + "/**/*.js";
     this.serverJSFilePath = parameters.server.serverJsFile;
     this.testPath = parameters.server.testPath + "/**/*.js";
@@ -272,14 +277,14 @@ gulp.task("server:watch", function() {
     gulp.watch(this.testPath, gulp.series(["server:test", "server:src-eslint", "server:test-eslint"]));
 });
 
-gulp.task("server:src-eslint", function() {
+gulp.task("server:src-eslint", function () {
     return gulp.src([parameters.server.srcPath + "/**/*.js"])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 
-gulp.task("server:test-eslint", function() {
+gulp.task("server:test-eslint", function () {
     return gulp.src([parameters.server.testPath + "/**/*.js"])
         .pipe(eslint())
         .pipe(eslint.format())
@@ -298,12 +303,12 @@ gulp.task("server:test-coverage", (cb) => {
 
 // ------------------------------- any other tasks ---------------------------------------
 
-gulp.task("other:copy-ansible-scripts", function() {
-    return gulp.src(parameters.other.ansibleScrptsPath + "/**/*", { "base": "./other/ansible" })
+gulp.task("other:copy-ansible-scripts", function () {
+    return gulp.src(parameters.other.ansibleScrptsPath + "/**/*", {"base": "./other/ansible"})
         .pipe(gulp.dest(parameters.other.ansibleDistFolder));
 });
 
-gulp.task("other:dist-clean", function() {
+gulp.task("other:dist-clean", function () {
     return del([parameters.other.distFolder]);
 });
 
@@ -352,9 +357,9 @@ gulp.task("checkin-ready", gulp.series("common:checkin-ready", "client:checkin-r
 
 gulp.task("test-coverage", (cb) => {
     exec("./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha -- --compilers js:babel-register -R spec " +
-    parameters.server.testPath + "/**/**/**/**/**/*.js " +
-    parameters.common.testPath + "/**/**/**/**/**/*.js " +
-    parameters.client.testPath + "/**/**/**/**/**/*.js " + parameters.client.testPath + "/**/**/**/**/**/*.jsx ", (err, stdout, stderr) => {
+        parameters.server.testPath + "/**/**/**/**/**/*.js " +
+        parameters.common.testPath + "/**/**/**/**/**/*.js " +
+        parameters.client.testPath + "/**/**/**/**/**/*.js " + parameters.client.testPath + "/**/**/**/**/**/*.jsx ", (err, stdout, stderr) => {
         console.log(stdout);
         console.log(stderr);
         cb(err);
